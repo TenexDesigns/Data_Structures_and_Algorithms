a8856f8d0640b8d055916c60e49d7cede79eb097
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-23 20:43:49 +0300

    Update 1_What_Is_Algorithm.py

--- Algorithms/1_What_Is_Algorithm.py
+++ Algorithms/1_What_Is_Algorithm.py
@@ -1,1 +1,142 @@
-ww
+
+
+Algorthim
+Masure performance of algorithm
+Time and Space complecity
+Big O notation
+
+Types of Algorthims
+ - Math algorithms
+ - Sort algoritms
+ - Search algorithms
+ - Misc . algorthims and problem solving algortims
+
+
+
+1. WHat is an algorithm
+- An algorithm is a set of well defined instructions to solve a particular problem
+
+input --> Algoritm --> Output
+
+e.g Algoritm to add two numbers
+
+input a,b   algorthm       output
+            add a +b       addition of a and b
+            return sum
+
+
+
+CHARACTERISTIC OF ALGORITMS
+
+- Have well defined inputs and outputs
+- Each step should be clear and unambiguous
+- Language independed
+
+
+
+Why Leran Algorthms
+
+As a developer , you are going to come across problems that you need to solve
+
+Learning algorithms translates to learning different techniques to efficently solve these problems
+
+One problem can be solved in many ways using different algoritms
+
+ Every algoritmn comes with its owm tradeoffs when it comes to performnce 
+
+There are multiple ways to solve one problem
+
+e.g There are multiple algorithms to sort a list of numbers
+
+How do we analize which one of them is the most efficent algoritm
+
+
+HOW DO WE MEASURE THE PERFORMANCE OF AN ALGORITM
+
+Algortim analis
+
+The absolute running time of an algoritm can  not be predicted , since it depends on a number of factors
+
+- Progrming language used to implement the algoritm
+
+- The computer the program runns on
+
+-Other programs running at the same time
+
+- Quality of the operating system
+
+
+
+
+
+PERFORMACE
+
+We evaluate the performance of an algorithm in terms of its input sixe.
+   An what the imput size does to the algoetim in terms of  time complexity and space complexity
+
+Time complexity - Amount of time taken by an algoritm to run, as a function of input size
+Space complexity - Amount of memory taken by an algoritm to run, as a function of input size
+
+
+
+By eveluateing performance of an algortm against the input size, the analis is not only machine independet , but the comparison is also more approprite
+
+Remeber , ther is no one solution that works every single time. It is always good to know multiple ways to solve the problem and use the best solution, given your constraints.
+
+
+  If your app need to be very quick and has plenty of memeory to work with, you dont have to worry about spac complexity
+
+If you have very little memeory to work with, you should pick a soulution that is reltively slower but need less space
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+HOW DO WE REPRENT THE TIME AND SPCE COLPLEXITY OF AN ALGORTHM
+We  represnt space and time complexity of an algorithm using ASMPTONIC OTATIONS
+
+ASYMPTONIC NOTATIONS - These are mathematical tools to represnent time and space complexity.
+
+ASYMPTIONC NOTATIONS ARE OF THREE TYPES
+-Big - O notation (O-notation) - WOrst case complexity
+-Omega Noation (^-Notataion) - Best case complexity
+-Theta noataion (o-Noatation) - Average case complexity
+
+
+We wil prialy focus on Big-O complexity or worst case complexity since that i what is mmostly asked 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
