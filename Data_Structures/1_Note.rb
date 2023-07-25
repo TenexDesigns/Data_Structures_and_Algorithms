3caa4639ac5d764415ca1179f35bfc6317e5cd0e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-25 20:40:14 +0300

    Create 1_Note.rb

---
+++ Data_Structures/1_Note.rb
@@ -0,0 +1,11 @@
+Different programming languages have different data strucure. 
+Our choise language will be javascript
+
+
+It is possible to implement a data strucure in a few different ways
+
+
+Please do not memorise the implementation , understand the concept
+
+
+  
