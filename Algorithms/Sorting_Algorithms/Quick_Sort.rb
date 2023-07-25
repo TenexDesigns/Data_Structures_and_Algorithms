96dbcc15680f35f37ace95dc089881e4d389501a
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-25 16:30:53 +0300

    Update Quick_Sort.rb

--- Algorithms/Sorting_Algorithms/Quick_Sort.rb
+++ Algorithms/Sorting_Algorithms/Quick_Sort.rb
@@ -92,4 +92,35 @@ This explanation should give you a solid understanding of Quick Sort algorithm i
 
 
 
+
+
+
+
+
+
+
+
+
+
+  ----------
+    function quickSort(arr){
+  if(arr.length <2)
+  {
+    return arr
+  }
+
+  let pivot = arr[arr.length -1]
+  let left = []
+  let right = []
+  for(let i = 0; i <arr .length -1 ;i++){
+    if( arr[i] < pivot){
+      left.push(arr[i])
+    } else {
+      right.push(arr[i])
+    }
+  }
+
+  return [ ...quickSort(left), pivot, .
+
+
 ...
