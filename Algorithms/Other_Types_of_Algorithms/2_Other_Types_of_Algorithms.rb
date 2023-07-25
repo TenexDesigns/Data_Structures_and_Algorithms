92b3c61af464308f65df7f2f2efe0a571790b12f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-25 18:14:44 +0300

    Create 2_Other_Types_of_Algorithms.rb

---
+++ Algorithms/Other_Types_of_Algorithms/2_Other_Types_of_Algorithms.rb
@@ -0,0 +1,51 @@
+
+Sure, there are many unique algorithms that are not directly related to search, sort, or math. Here are a few examples:
+
+1. Text Compression Algorithms:
+Text compression algorithms are used to reduce the size of text files without losing the original information. Huffman coding and Lempel-Ziv-Welch (LZW) are examples of such algorithms. Huffman coding is a lossless data compression algorithm that is used in applications like file compression (e.g., ZIP files) and transmission (e.g., JPEG images). LZW is also a lossless data compression algorithm and is the basis for the GIF image format.
+
+2. Cryptographic Algorithms:
+Cryptographic algorithms are used to secure data and communications. They include symmetric key algorithms (like AES and
+
+
+Recipe Algorithms:
+  Just like sorting papers and even tying your shoes, following a recipe is a type of algorithm. The goal, of course, is to create a duplicated outcome. In order to complete a recipe, you have to follow a given set of instructions. For instance, when you are making bread, you need flour, yeast, and water. After you have your ingredients, you need to combine them in a certain way that will create a predictable outcome, in this case, a loaf of bread invisibly.com.
+
+    
+    Sorting Papers Algorithm:
+    When you are sorting office files or your personal documents, you are implementing an algorithm. In its most basic sense, you are following a set of steps to achieve an outcome. The reason why sorting papers is a great example is that it shows the variety of tasks and specifications algorithms can use. For instance, you can sort your files alphabetically, by word count, by date, and countless others. The goal is to simplify the organizational process by using small tasks invisibly.com.
+
+      
+      Cryptography Algorithms:
+    These include Advanced Encryption Standard (AES), winner of NIST competition, also known as Rijndael, and Data Encryption Standard (DES), sometimes DE Algorithm, winner of NBS selection competition, replaced by AES for most purposes en.wikipedia.org.
+
+        
+        Graph Algorithms: 
+      These include Hopcroft–Karp algorithm, Hungarian algorithm, Prüfer coding, Tarjans off-line lowest common ancestors algorithm, and Topological sort en.wikipedia.org.
+
+        Computer Algebra Algorithms:
+        Some examples are Cantor–Zassenhaus algorithm, Faugère F4 algorithm, Gospers algorithm, Knuth–Bendix completion algorithm, Pollards kangaroo algorithm, and Risch algorithm en.wikipedia.org.
+
+        Number Theoretic Algorithms: 
+        These include Chakravala method, Extended Euclidean algorithm, Integer factorization, Multiplication algorithms, Modular square root, Odlyzko–Schönhage algorithm, and Lenstra–Lenstra–Lovász algorithm en.wikipedia.org.
+
+
+
+      Quantum Algorithms:
+        These include Deutsch–Jozsa algorithm, Grover's algorithm, Shor's algorithm, and Simons algorithm. Quantum algorithms are designed for quantum computers which use quantum bits, or qubits, instead of classical bits for computation. Quantum algorithms can solve certain problems much faster than classical algorithms en.wikipedia.org.
+
+          Networking Algorithms:
+            These include Karn's algorithm and Nagle's algorithm. Karns algorithm addresses the problem of getting accurate estimates of the round-trip time for messages when using TCP, while Nagles algorithm improves the efficiency of TCP/IP networks by coalescing packets en.wikipedia.org.
+
+              Sequence Permutations Algorithms: 
+                These include Fisher–Yates shuffle (also known as the Knuth shuffle), Schensted algorithm, Steinhaus–Johnson–Trotter algorithm (also known as the Johnson–Trotter algorithm), and Heaps permutation generation algorithm. These algorithms are used to generate, shuffle, or manipulate sequences or permutations in different ways en.wikipedia.org.
+
+              Subsequences Algorithms: 
+                These include algorithms for the longest common subsequence problem, longest increasing subsequence problem, Ruzzo–Tompa algorithm, and shortest common supersequence problem. These algorithms are used to find subsequences with certain properties within a sequence en.wikipedia.org.
+
+                  Unique Random Data Generation: 
+                    This is a practical example of algorithm analysis where a method generates unique random values inside a provided range dev.to.
+
+                  Remember, the field of algorithms is vast and it intersects with many areas of computer science and mathematics. Hence, there are many unique algorithms to learn and explore.
+
+  
