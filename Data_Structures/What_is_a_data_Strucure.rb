7fc559ed2770f33c2bc746082c10155ae16f5f23
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-25 20:49:00 +0300

    Update What_is_a_data_Strucure.rb

--- Data_Structures/What_is_a_data_Strucure.rb
+++ Data_Structures/What_is_a_data_Strucure.rb
@@ -1,1 +1,65 @@
-vvv
+What is a data strucure
+
+A data strucure is a way to store and organise data so that it can be used efficently
+A data strucure is a collection of data values , the relationships among them and the functions or operations that can be applied to that data.
+
+
+
+
+
+Why Learn Data strucure
+The differnece between a function taking a few milliseconds  vs a few seconds or even minutes comes dwn to the selection of the right data structure.
+
+
+Data Strucure help you solve problems in a more effcient way, both in terms of time and memory.
+
+
+Built in data strucures in Javascript
+
+Arrays
+Objects
+Sets
+Maps
+
+
+
+
+Custome data Strucures
+
+Stacks
+Queues
+Circular queues
+Linked listes
+Hash tables
+Tress
+Graphs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
