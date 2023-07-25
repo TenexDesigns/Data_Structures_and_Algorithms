24cd7574f0d46d6d394ea9b3930923ef48f798bb
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-25 16:20:22 +0300

    Create Quick_Sort.rb

---
+++ Algorithms/Sorting_Algorithms/Quick_Sort.rb
@@ -0,0 +1,95 @@
+Quick Sort Algorithm:
+
+Quick Sort is a widely used comparison-based sorting algorithm that follows the divide and conquer approach. It chooses a pivot element from the array and partitions the other elements into two sub-arrays according to whether they are less than or greater than the pivot. The sub-arrays are then sorted recursively.
+
+Heres the implementation of the Quick Sort algorithm in JavaScript:
+
+function quickSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+const arr = [64, 34, 25, 12, 22, 11, 90];
+console.log(quickSort(arr)); // Output: [11, 12, 22, 25, 34, 64, 90]
+Time Complexity:
+
+The time complexity of the Quick Sort algorithm varies depending on the choice of the pivot element. In the average and best-case scenarios, the time complexity is O(n log n). However, in the worst-case scenario (when the pivot is consistently the smallest or largest element), the time complexity is O(n^2). Despite this worst-case behavior, Quick Sort is often faster in practice than other O(n log n) sorting algorithms like Merge Sort and Heap Sort.
+
+Effective Implementation:
+
+To make Quick Sort more effective:
+
+Choose the Pivot Wisely: Choosing the right pivot element is crucial to achieve good performance in Quick Sort. The ideal choice of pivot would be the median of the array, but finding the exact median can be time-consuming. Common techniques include choosing the first, last, or middle element as the pivot, or using random pivot selection to improve the average-case performance.
+
+Use Insertion Sort for Small Subarrays: For small subarrays, using a simpler sorting algorithm like Insertion Sort can be more efficient than recursively applying Quick Sort. Implement a cutoff for small subarrays and use Insertion Sort for them.
+
+Randomize the Pivot: Random pivot selection can reduce the chance of consistently hitting the worst-case performance and generally results in better average-case performance.
+
+Quick Sort is a very efficient sorting algorithm when implemented effectively. However, its worst-case behavior and sensitivity to pivot choice make it important to use randomization and other techniques to ensure good performance across various scenarios.
+
+
+
+
+MORE EXPLAANNTION
+  ***********************************************************************************************************************
+
+  The Quick Sort algorithm is based on the Divide and Conquer concept. Here is how it works:
+
+function quickSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+    let pivot = array[0];
+    let left = [];
+    let right = [];
+    for (let i = 1; i < array.length; i++) {
+        array[i] < pivot ? left.push(array[i]) : right.push(array[i]);
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+let myArray = [3, 7, 2, 5, 1, 4, 6, 8];
+console.log(quickSort(myArray)); // Output: [1,2,3,4,5,6,7,8]
+Step-by-Step Explanation:
+
+The algorithm starts with the function quickSort which takes array as input.
+If the length of array is less than or equal to 1, its already sorted, so the function returns the array.
+The first element of the array is chosen as pivot. The choice of pivot can vary (first element, last, middle, median etc.), and its a subject of optimization.
+Two empty arrays left and right are created.
+Then, each element of the array (from the second one to the last) is compared to the pivot: if its less, it goes to the left array, if its greater, it goes to right.
+Now, quickSort is called recursively for both left and right, and the result is combined with the pivot to form a new array which is returned.
+This process is performed repeatedly until the entire array is sorted.
+
+Time Complexity:
+
+The best-case and average-case time complexity of Quick Sort is O(n log n), where n is the number of elements in the array. This is achieved when the pivot always splits the array into two nearly equal halves.
+
+The worst-case time complexity is O(n^2) which happens when the pivot is the smallest or the largest element in the array, causing an unbalanced partitioning.
+
+However, this worst-case scenario is extraordinarily unlikely to occur if the pivot is chosen carefully - ideally, it should be median. Even a randomly chosen pivot results in a good expected time complexity of O(n log n).
+
+Optimizations:
+
+Pivot selection can be optimized in several ways - one widely used is to take the median of the first, middle, and last elements of the array.
+For smaller array sizes, quicksort doesnt perform that well in comparison to simpler sorting algorithms. Hence, when the size of the array becomes small during recursion, the algorithm can switch to simpler ones like Insertion Sort.
+The algorithm can be optimized to sort 'in-place'. Using additional space with arrays left and right is good for understanding, but not necessary - all operations can be performed within the original array. This optimization is useful in memory-constrained environments.
+This explanation should give you a solid understanding of Quick Sort algorithm in JavaScript! Happy coding!
+
+
+
+
+
+...
