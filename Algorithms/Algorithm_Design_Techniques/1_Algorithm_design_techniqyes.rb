fae431944a2ed0a6c9209c3f85a8aadb3364f7e0
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-25 20:15:27 +0300

    Create 1_Algorithm_design_techniqyes.rb

---
+++ Algorithms/Algorithm_Design_Techniques/1_Algorithm_design_techniqyes.rb
@@ -0,0 +1,59 @@
+Algorithm Design Techniques are methods or approaches used to create efficient and effective algorithms to solve specific problems. Some of the common algorithm design techniques include:
+
+Greedy Algorithms:
+Greedy algorithms make locally optimal choices at each step with the hope of finding a global optimum solution. These algorithms are often used for optimization problems. Examples of greedy algorithms include:
+
+Kruskals algorithm for finding the Minimum Spanning Tree of a graph.
+Dijkstras algorithm for finding the shortest path in a graph with non-negative weights.
+
+  
+Divide and Conquer:
+Divide and Conquer is a strategy where a problem is divided into smaller subproblems, solved independently, and then combined to get the final solution. This technique is widely used in sorting and searching algorithms. Examples include:
+
+Merge Sort: It divides the array into two halves, sorts each half separately, and then merges the sorted halves.
+Quick Sort: It chooses a pivot, partitions the array around the pivot, and recursively sorts the subarrays.
+
+
+  
+Dynamic Programming:
+Dynamic Programming is a method to solve complex problems by breaking them down into simpler subproblems and solving each subproblem only once. It stores the solutions of subproblems in a data structure and builds up to solve the original problem efficiently. Examples include:
+
+Fibonacci sequence using memoization or tabulation to avoid redundant calculations.
+Longest Common Subsequence problem.
+
+
+  
+Backtracking:
+Backtracking is a method to solve problems incrementally by trying out different possibilities and undoing choices that do not lead to a valid solution. Examples include:
+
+N-Queens problem: Placing N queens on an N x N chessboard without them attacking each other.
+Sudoku puzzle solver.
+
+
+  
+Recursive Algorithms:
+Recursive algorithms are algorithms that call themselves with smaller instances of the problem to be solved. Examples include:
+
+Binary search on a sorted array.
+Tower of Hanoi problem.
+Permutations and Combinations.
+
+  
+Branch and Bound:
+Branch and Bound is a method to solve optimization problems by systematically searching the solution space and using bounding techniques to eliminate suboptimal solutions. Examples include:
+
+Traveling Salesman Problem.
+Knapsack problem.
+
+  
+Randomized Algorithms:
+Randomized algorithms introduce randomness in their computations to achieve a more efficient solution or to approximate a solution with high probability. Examples include:
+
+Randomized Quick Sort: Choosing a random pivot in Quick Sort.
+Las Vegas algorithm for finding the median of an array.
+These are just some of the algorithm design techniques used to create efficient and effective algorithms. Depending on the problem at hand, a combination of these techniques or other specific approaches may be required to devise the most suitable algorithm.
+
+
+
+
+  
