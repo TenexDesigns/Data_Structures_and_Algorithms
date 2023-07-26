0e08a717501fe7c1679bcf04a35bbf0a6bd41745
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-26 14:02:19 +0300

    Create Hash_Table.rb

---
+++ Data_Structures/Custom_Data_Strucures/Hash_Table.rb
@@ -0,0 +1,172 @@
+A hash table is a data structure that allows for efficient storage and retrieval of key-value pairs. It is also known as a hash map or associative array. Hash tables are widely used in computer science and programming due to their ability to provide fast access to data based on keys.
+
+Key features of a hash table:
+
+Hash Function: The core component of a hash table is the hash function. It takes a key as input and converts it into an index (or hash code) in the underlying array where the corresponding value will be stored. An effective hash function should produce a unique index for each key, but collisions (when two different keys produce the same index) can occur.
+
+Array Storage: A hash table uses an array to store the key-value pairs. Each slot in the array is often called a bucket or slot.
+
+Collision Handling: Since collisions can occur when multiple keys map to the same index, hash tables use collision resolution techniques to handle such situations. There are different approaches, including chaining (using linked lists to store multiple key-value pairs at the same index) and open addressing (probing for an empty slot when a collision occurs).
+
+Fast Access: One of the main advantages of hash tables is their ability to provide constant-time (O(1)) access to values based on their keys (on average). This makes hash tables an efficient data structure for lookups, insertions, and deletions.
+
+Dynamic Size: Hash tables can dynamically resize themselves to maintain a balance between the number of elements and the number of slots, which helps to optimize performance.
+
+Load Factor: The load factor of a hash table is the ratio of the number of stored elements to the number of available slots. A low load factor means fewer collisions and better performance, while a high load factor may lead to increased collisions and slower operations.
+
+Usage and Benefits of Hash Tables:
+
+Hash tables are commonly used for implementing data caches, symbol tables, databases, and in various algorithms like hashing-based searching and indexing.
+They provide fast access to data and efficient insertion and deletion operations.
+Hash tables are valuable when you have a large dataset and need to quickly find or update information based on keys.
+They are also utilized in various programming languages and libraries to provide dictionary-like data structures.
+Heres a simple implementation of a hash table in JavaScript using separate chaining for collision resolution:
+
+class HashTable {
+  constructor(size = 10) {
+    this.size = size;
+    this.table = new Array(size);
+  }
+
+  hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * (i + 1)) % this.size;
+    }
+    return hash;
+  }
+
+  put(key, value) {
+    const index = this.hash(key);
+    if (!this.table[index]) {
+      this.table[index] = [];
+    }
+    this.table[index].push({ key, value });
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    if (this.table[index]) {
+      for (const item of this.table[index]) {
+        if (item.key === key) {
+          return item.value;
+        }
+      }
+    }
+    return undefined;
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    if (this.table[index]) {
+      this.table[index] = this.table[index].filter(item => item.key !== key);
+    }
+  }
+}
+
+// Example usage:
+const myHashTable = new HashTable();
+myHashTable.put("name", "John");
+myHashTable.put("age", 30);
+
+console.log(myHashTable.get("name")); // Output: "John"
+console.log(myHashTable.get("age")); // Output: 30
+
+myHashTable.remove("age");
+console.log(myHashTable.get("age")); // Output: undefined
+In the example above, we implemented a hash table with methods for putting (inserting) key-value pairs, getting (retrieving) values based on keys, and removing key-value pairs. The implementation uses a simple hash function and separate chaining for handling collisions.
+
+
+
+
+
+
+
+
+MORE EXPLANANTION
+******************************************************************************************************************************************
+
+
+Hash table collisions occur when different keys are hashed to the same index in the hash table. There are several techniques to handle these collisions including:
+
+Separate Chaining: This technique involves creating a linked list of key-value pairs for each index in the hash table.
+  If multiple keys hash to the same index, their key-value pairs are appended to the end of the linked list geeksforgeeks.org. Heres a Javascript example of how this can be implemented:
+    class HashTable {
+        constructor() {
+            this.table = new Array(127);
+            this.size = 0;
+        }
+
+        _hash(key) {
+            let hash = 0;
+            for (let i = 0; i < key.length; i++) {
+                hash += key.charCodeAt(i);
+            }
+            return hash % this.table.length;
+        }
+
+        set(key, value) {
+            const index = this._hash(key);
+            if (this.table[index]) {
+                for (let i = 0; i < this.table[index].length; i++) {
+                    if (this.table[index][i][0] === key) {
+                        this.table[index][i][1] = value;
+                        return;
+                    }
+                }
+                this.table[index].push([key, value]);
+            } else {
+                this.table[index] = [];
+                this.table[index].push([key, value]);
+            }
+            this.size++;
+        }
+    }
+    ```
+Linear Probing: This technique involves finding the next available slot in the hash table when a collision occurs. This is done by incrementing the index by 1 until an empty slot is found baeldung.com.
+Heres a Javascript example of how this can be implemented:
+    class HashTable {
+        constructor() {
+            this.table = new Array(127);
+            this.size = 0;
+        }
+
+        _hash(key) {
+            let hash = 0;
+            for (let i = 0; i < key.length; i++) {
+                hash += key.charCodeAt(i);
+            }
+            return hash % this.table.length;
+        }
+
+        set(key, value) {
+            let index = this._hash(key);
+            while(this.table[index]) {
+                index++;
+            }
+            this.table[index] = [key, value];
+            this.size++;
+        }
+    }
+    
+Quadratic Probing: This is a variant of linear probing where instead of incrementing the index by 1, it is incremented by a quadratic function (i.e., by 1, 4, 9, 16, etc.) until an empty slot is found geeksforgeeks.org.
+Double Hashing: This technique involves using a secondary hash function to find another slot in the table when a collision occurs. This can help avoid clustering, which can be a problem with linear and quadratic probing geeksforgeeks.org.
+The choice of collision resolution technique depends on various factors such as the expected number of keys, the load factor of the hash table, and the distribution of the keys. For example, separate chaining can be a good choice if the hash table is expected to have a high load factor, while linear probing might be more efficient for lower load factors stackoverflow.com.
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+..
