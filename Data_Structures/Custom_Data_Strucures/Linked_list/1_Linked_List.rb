2dc70076f5b628df1102b10ed27a9f681f850e55
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-26 17:52:26 +0300

    Update 1_Linked_List.rb

--- Data_Structures/Custom_Data_Strucures/Linked_list/1_Linked_List.rb
+++ Data_Structures/Custom_Data_Strucures/Linked_list/1_Linked_List.rb
@@ -1,14 +1,31 @@
 A linked data structure is a type of data structure that consists of a collection of elements called nodes, where each node points to the next node in the sequence. Linked data structures are used to store and organize data in a way that allows for efficient insertion, deletion, and traversal of elements. There are several types of linked data structures, including singly linked lists, doubly linked lists, and circular linked lists.
 
+A linked list is a linear data strucure that inliced a series of connected nodes
+  Each node consits of a data value and a pointter that points to th next node
+
+  The list elemnts can be easily inserted or removed without reallocation or reorgnisation of the entire strucure
+
+  Random acccess of elemnts is not feasible and accessing an elemnt  has linear time complexity.
+
+  The linked list data strucure supports three main operations
+     -> Insertion - To add an elemnt at the begining and end or at a given index in the list
+     -> Deletion - to remove an item given its index or value
+     -> Search - to find an  elemnt given its value.
+
+  Linked List Usage
+
+All applications of both stacks and queuue are applications of linked listes
+Also Image vieww
+  
 Here are some key points about linked data structures:
 
 Node: Each element in a linked data structure is represented by a node. The node contains two parts: the data (or value) that represents the element and a reference (or pointer) to the next node in the sequence.
 
 Singly Linked List: In a singly linked list, each node points to the next node in the sequence, forming a unidirectional chain. The last node points to null, indicating the end of the list.
 
-Doubly Linked List: In a doubly linked list, each node points to both the next and the previous node in the sequence, forming a bidirectional chain. The first node's previous pointer and the last node's next pointer point to null.
+Doubly Linked List: In a doubly linked list, each node points to both the next and the previous node in the sequence, forming a bidirectional chain. The first nodes previous pointer and the last nodes next pointer point to null.
 
-Circular Linked List: A circular linked list is a variation of a singly or doubly linked list in which the last node points back to the first node (in case of singly linked circular list) or the first node's previous pointer points to the last node (in case of doubly linked circular list).
+Circular Linked List: A circular linked list is a variation of a singly or doubly linked list in which the last node points back to the first node (in case of singly linked circular list) or the first nodes previous pointer points to the last node (in case of doubly linked circular list).
 
 Dynamic Size: Linked data structures allow for dynamic sizing, meaning elements can be easily added or removed without the need to resize the structure.
 
@@ -20,10 +37,8 @@ Random Access: Unlike arrays, linked data structures do not provide constant-tim
 
 Linked data structures are widely used in computer science and programming for various applications, such as implementing data caches, managing memory, representing graphs and trees, and implementing various data structures like stacks and queues.
 
-Here's a simple example of implementing a singly linked list in JavaScript:
+Heres a simple example of implementing a singly linked list in JavaScript:
 
-javascript
-Copy code
 class Node {
   constructor(data) {
     this.data = data;
