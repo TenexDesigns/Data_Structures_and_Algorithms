3eee0ba1b0040ef0bf8918c9085fad58d8417868
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-26 11:38:35 +0300

    Create Queue.rb

---
+++ Data_Structures/Custom_Data_Strucures/Queue.rb
@@ -0,0 +1,25 @@
+
+Queue
+
+
+The queue data strucure is a sequential collection of elemnts that follows the principle of First In First Out
+
+The first elemnt inserted into the queue is first elemnt to be removed
+
+
+
+
+ Analogy -----> A queueu of people . Peoople eneter the queueu at one end  and then leave the queuue from the other end
+
+Queue is an abstract data type. It is defined by its behaviour rather than being a mathematical model
+
+
+The Queueu data struture supports two main operations
+Enquque - which adds an element  to  the  end of the collection
+Dequeue - which removes an elemennt  from the head of collection
+
+
+
+
+
+
