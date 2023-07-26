e535cd885199962c952d6101d1e651a7792a3fe5
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-26 08:10:59 +0300

    Create Maps.rb

---
+++ Data_Structures/Built_in_Data_Strucures_in_Java_Script/Maps.rb
@@ -0,0 +1,20 @@
+Map
+
+A map is an unordered collection of key-value pairs.
+  Both keys and vlaues can be of any data type.
+
+To retrieve a value, you can use the corresponding key.
+  Maps are iterables. They can be used with a for of loop.
+
+
+
+OBJECTS VS MAPS
+
+    Onjectsa are unordered where as maps are Orderred
+
+    Keys in objects can  only be strings or symbol types, where as in maps they can be of any type
+    An object has a prototype ans may contain a few  defaul keys which may collide with 
+
+
+
+
