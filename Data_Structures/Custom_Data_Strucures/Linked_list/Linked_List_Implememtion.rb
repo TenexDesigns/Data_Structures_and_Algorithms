406bdc890a9825c8c9e58e86048b81820b8a5a2e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-26 21:01:32 +0300

    Update Linked_List_Implememtion.rb

--- Data_Structures/Custom_Data_Strucures/Linked_list/Linked_List_Implememtion.rb
+++ Data_Structures/Custom_Data_Strucures/Linked_list/Linked_List_Implememtion.rb
@@ -271,6 +271,286 @@ In conclusion, these are the fundamental methods you would implement for a linke
 
 
 
+in the above context also explain with code samples how to preppend and append an elemt, search for an elemt in liked ist, do reverser  in linked list, what tail and head for apprendind prepeding, liked list stack and queue
+----------------------------------------------------------------------------------------------------------------------------------------
+    
+
+  Sure! Lets add the implementations for appending, prepending, searching, reversing a linked list, and explain tail and head concepts for appending and prepending. Well also discuss linked list as a stack and queue.
+
+1.Append an Element (Add to the End):
+      
+Appending an element means adding it at the end of the linked list. To do this, we can traverse the list until we reach the last node and then add the new element as the next of the last node.
+
+javascript
+Copy code
+// linkedlist class
+class LinkedList {
+    // ... (existing code)
+
+    // Append an element to the end of the list
+    append(element) {
+        const node = new Node(element);
+        if (!this.head) {
+            this.head = node;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.size++;
+    }
+}
+
+// Test appending an element to the linked list
+const ll = new LinkedList();
+ll.add(10);
+ll.add(20);
+
+// Output: 10 20
+ll.printList();
+
+ll.append(30);
+ll.append(40);
+
+// Output: 10 20 30 40
+ll.printList();
+
+
+
+      
+2.Prepend an Element (Add to the Beginning):
+Prepending an element means adding it at the beginning of the linked list. To do this, we can make the new node the new head and point its next to the current head.
+
+javascript
+Copy code
+// linkedlist class
+class LinkedList {
+    // ... (existing code)
+
+    // Prepend an element to the beginning of the list
+    prepend(element) {
+        const node = new Node(element);
+        node.next = this.head;
+        this.head = node;
+        this.size++;
+    }
+}
+
+// Test prepending an element to the linked list
+const ll = new LinkedList();
+ll.add(20);
+ll.add(30);
+
+// Output: 20 30
+ll.printList();
+
+ll.prepend(10);
+
+// Output: 10 20 30
+ll.printList();
+
+
+
+
+
+  
+3.Search for an Element in the Linked List:
+  
+Searching for an element in the linked list involves traversing the list and comparing each nodes element with the target element until a match is found.
+
+javascript
+Copy code
+// linkedlist class
+class LinkedList {
+    // ... (existing code)
+
+    // Search for an element in the list
+    search(element) {
+        let current = this.head;
+        while (current) {
+            if (current.element === element) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+}
+
+// Test searching for an element in the linked list
+const ll = new LinkedList();
+ll.add(10);
+ll.add(20);
+ll.add(30);
+
+console.log(ll.search(20)); // Output: true
+console.log(ll.search(40)); // Output: false
+
+
+
+
+
+  
+4.Reverse the Linked List:
+Reversing the linked list involves changing the direction of the next pointers of each node. We start by storing the previous, current, and next nodes while iterating through the list and update the next pointers accordingly.
+
+javascript
+Copy code
+// linkedlist class
+class LinkedList {
+    // ... (existing code)
+
+    // Reverse the list
+    reverse() {
+        let prev = null;
+        let current = this.head;
+        while (current) {
+            const next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        this.head = prev;
+    }
+}
+
+// Test reversing the linked list
+const ll = new LinkedList();
+ll.add(10);
+ll.add(20);
+ll.add(30);
+
+// Output: 30 20 10
+ll.reverse();
+ll.printList();
+
+
+
+
+
+
+
+
+  
+5.Tail and Head Concepts for Appending and Prepending:
+  
+In the context of linked lists, the tail refers to the last node of the list, and the head refers to the first node of the list. By maintaining references to the head and optionally the tail, we can easily perform operations like appending and prepending.
+
+In the existing LinkedList class, head is already used to keep track of the first node. However, to efficiently append elements without traversing the entire list every time, we can also keep track of the tail. Heres how we can do it:
+
+javascript
+Copy code
+// linkedlist class
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null; // Keep track of the tail
+        this.size = 0;
+    }
+
+    // Append an element to the end of the list
+    append(element) {
+        const node = new Node(element);
+        if (!this.head) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.size++;
+    }
+}
+
+// Test appending an element to the linked list with tail
+const ll = new LinkedList();
+ll.add(10);
+ll.add(20);
+
+// Output: 10 20
+ll.printList();
+
+ll.append(30);
+ll.append(40);
+
+// Output: 10 20 30 40
+ll.printList();
+
+
+
+
+
+
+  
+6.Linked List as a Stack and Queue:
+A linked list can be used to implement both a stack and a queue efficiently.
+
+(a) -----> Linked List as a Stack:
+For a stack, we can use the prepend() method to push elements onto the stack (add to the top) and the removeFrom(0) method to pop elements from the stack (remove from the top).
+javascript
+Copy code
+// linkedlist class
+class LinkedList {
+    // ... (existing code)
+
+    // Implementing Linked List as a Stack
+    push(element) {
+        this.prepend(element);
+    }
+
+    pop() {
+        return this.removeFrom(0);
+    }
+}
+
+// Test Linked List as a Stack
+const stack = new LinkedList();
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+console.log(stack.pop()); // Output: 30
+console.log(stack.pop()); // Output: 20
+console.log(stack.pop()); // Output: 10
+
+
+
+
+
+  
+(b) ------> Linked List as a Queue:
+For a queue, we can use the append() method to enqueue elements (add to the rear) and the removeFrom(0) method to dequeue elements (remove from the front).
+javascript
+Copy code
+// linkedlist class
+class LinkedList {
+    // ... (existing code)
+
+    // Implementing Linked List as a Queue
+    enqueue(element) {
+        this.append(element);
+    }
+
+    dequeue() {
+        return this.removeFrom(0);
+    }
+}
+
+// Test Linked List as a Queue
+const queue = new LinkedList();
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+
+console.log(queue.dequeue()); // Output: 10
+console.log(queue.dequeue()); // Output: 20
+console.log(queue.dequeue()); // Output: 30
+With these implementations, you now have a fully functional Linked List class with methods for appending, prepending, searching, reversing, and using it as a stack and queue. You can use these methods to build more complex data structures and solve various problems that require linked lists.
+
+
+
 
 
 
