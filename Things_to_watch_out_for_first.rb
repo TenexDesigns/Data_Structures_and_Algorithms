6485e148af5904cc502ca4f2f9a98e805b181048
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-27 10:12:47 +0300

    Create Things_to_watch_out_for_first.rb

---
+++ Things_to_watch_out_for_first.rb
@@ -0,0 +1,140 @@
+Top 5 datastrucres to lears
+
+
+1.Heap
+-max heap 
+-min heap
+-implement heaps using arrays and heapyfy
+-when to use heap in questions - when you need quick access to max or min e.g use heaps to implememnt priority queue
+- e.g of algorithms usning heap .e.g Heap sort, dijkstarts algorithm, median of stream algorithm
+
+
+2.Binary Tree
+-Root node, leaf node
+-use cases - data base indexing, sorting algortihm, decision trees
+- when to use binary tree in questions - ,when the questions instructs you to,
+- Binary eees are greast candinates to use for recursion
+-Learn tree transversal 
+   e.g  -pre-order : 
+        -post-order
+        -in-order
+        -Level -order
+-Binary tree problems - Lowest common ancestor
+                      - Print left view of a binary tree
+                      - Binary tree maximum path sum
+
+
+
+
+
+
+
+3.Hash map or Hash table 
+- Takes in key and conerts it to index using hash function
+- When to use it - When  you need to access information really quickly without using muc work
+- Quick look ups in hash map come at the cost of extra space, so always thin if there are better solutions that do not us extra space
+- top k problem
+
+
+
+4. Stack and queueu and  Linked lists
+
+-stack - pop
+      - push
+
+-queue - enqueue
+      - dequeuu 
+
+- STacks and queuues are impleented using arrays or Linked lists
+- when to use it - When you have to access some previously seen values and you need to access the last seen value
+-problems - Lagest rectangle in a histogram
+
+
+
+
+5. Grapghs
+- Used in many questions in coding interviews
+-Vertices and edges
+- directed and Undirected graph
+- when to use - when you spot relationships between different entities in a  problem, use graph
+-Learn about - Graph search
+                    -Graph search algorithms
+                        - Depth first search
+                        - Brath first search
+                        - Dijikstras algorithm
+            - Topological sort
+           - Loop in graph
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ALGORITHMS TO LERAN
+
+6.Sliding window
+7.Binary serach
+8. Depth fiest serach and Breadth first search
+8.Recursion - incliding trees, graphs, backtracking, dp ,and more, what it does, how it is used, it takes more memeory
+9.Hash maps. used on two sum
+10.Dynamic programin
+11.System design  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
