903007d7f826821c0b0fb3058a7ad64da2d689ed
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-28 18:14:08 +0300

    Update 3_Stack_Using_Linked_List.rb

--- Data_Structures/Custom_Data_Strucures/Linked_list/3_Stack_Using_Linked_List.rb
+++ Data_Structures/Custom_Data_Strucures/Linked_list/3_Stack_Using_Linked_List.rb
@@ -96,6 +96,157 @@ stack.print(); // Output: 10 20
 
 
 
+----------v-----v-----v-----v---------------------------------------------------------------------------------------------------------
+
+  This is the Linked List I Used or the one below this, I dont know which is which
+
+  javascript
+Copy code
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedListWithTail {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    getSize() {
+        return this.size;
+    }
+
+    prepend(value) {
+        const node = new Node(value);
+        if (this.isEmpty()) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++;
+    }
+
+    append(value) {
+        const node = new Node(value);
+        if (this.isEmpty()) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.size++;
+    }
+
+    insert(value, index) {
+        if (index < 0 || index > this.size) {
+            return console.log("Index is out of bound");
+        } else if (index === 0) {
+            this.prepend(value);
+        } else if (index === this.size) {
+            this.append(value);
+        } else {
+            const node = new Node(value);
+            let prev = this.head;
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            node.next = prev.next;
+            prev.next = node;
+            this.size++;
+        }
+    }
+
+
+    deleteFirst() {
+        if (this.isEmpty()) {
+            return console.log("List is empty, nothing to delete.");
+        }
+
+        if (this.head === this.tail) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+        }
+
+        this.size--;
+    }
+
+
+
+
+            
+    deleteLast() {
+        if (this.isEmpty()) {
+            return console.log("List is empty, nothing to delete.");
+        }
+
+        if (this.head === this.tail) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            let prev = this.head;
+            while (prev.next !== this.tail) {
+                prev = prev.next;
+            }
+            prev.next = null;
+            this.tail = prev;
+        }
+
+        this.size--;
+    }
+
+
+    removeFrom(index) {
+        let removedNode;
+        if (index < 0 || index >= this.size) {
+            return console.log("Index is out of bound");
+        } else if (index === 0) {
+            removedNode = this.head;
+            this.head = this.head.next;
+            if (this.size === 1) {
+                this.tail = null;
+            }
+        } else {
+            let prev = this.head;
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            removedNode = prev.next;
+            prev.next = removedNode.next;
+            if (index === this.size - 1) {
+                this.tail = prev;
+            }
+        }
+        this.size--;
+        return removedNode.value;
+    }
+
+    print() {
+        if (this.isEmpty()) {
+            console.log("This List is Empty");
+        } else {
+            let curr = this.head;
+            let listValues = "";
+            while (curr) {
+                listValues += `${curr.value} `;
+                curr = curr.next;
+            }
+            console.log(listValues);
+        }
+    }
+}
 
 
 
@@ -108,12 +259,241 @@ stack.print(); // Output: 10 20
 
 
 
+or this one , I dont know which is which
+----------v-----v-----v-----v---------------------------------------------------------------------------------------------------------
+
+  This is the Linked List I Used
 
 
 
 
 
+class Node{
 
+    constructor(value){
+        this.value = value
+        this.next = null
+
+    }
+
+}
+
+
+
+class LinkedList{
+
+    constructor(){
+        this.head = null
+        this.size=0
+    }
+
+    isEmpty(){
+        return this.size === 0
+    }
+
+    getSize(){
+        return this.size
+    }
+
+    // O(1) - Constant time complexity
+    prepend(value){
+        const node = new Node(value)
+
+        if(this.isEmpty()){
+            this.head =node
+
+        } else{
+            node.next = this.head
+            this.head = node
+        }
+        this.size++ 
+
+    }
+
+    // O(n) - Linear time complexity
+    append(value){
+        const node = new Node(value)
+
+        if(this.isEmpty()){
+            this.head = node
+        } else{
+            let prev = this.head
+
+            while(prev.next){
+                prev = prev.next
+
+            }
+            prev.next = node
+        }
+        this.size++
+
+    }
+
+    insert(value,index){
+        if(index < 0){
+            return console.log("Index is less than zero")
+        }else if(index > this.size){
+            return console.log("Index is out of bound")
+
+
+        }else if(index ===0){
+            const node = new Node(value);
+            node.next = this.head;
+            this.head = node;
+
+        }
+        
+        else{
+            const node = new Node(value)
+            let prev = this.head
+
+
+            for(let i = 0; i< index-1;i++){
+                prev = prev.next
+            }
+
+            node.next = prev.next
+            prev.next = node
+
+          
+
+        }
+        this.size++
+
+
+
+
+    }
+    removeFrom(index){
+        let removedNode;
+        if(index < 0){
+            return console.log("Index is less than zero")
+        }else if(index > this.size){
+            return console.log("Index is out of bound")
+        }else if(index ===0){
+            removedNode = this.head
+            this.head = this.head.next
+
+          
+        }        
+        else{
+           
+            let prev = this.head
+
+
+            for(let i = 0; i< index-1;i++){
+                prev = prev.next
+            }
+
+           removedNode= prev.next
+           prev.next = removedNode.next
+          
+         
+
+          
+
+        }
+      
+
+        this.size--
+        return removedNode.value
+
+
+   
+   
+    }
+
+    search(value){
+        if(this.isEmpty()){
+            return console.log("This list is empty")
+        }
+
+        let i =0
+        let cur = this.head
+
+        while(cur){
+            if(cur.value === value){
+                return console.log(`The index is ${i}`)
+            }
+            cur = cur.next
+            i++
+        }
+
+        return console.log("The value could not be found")
+
+    }
+
+    //O(n)
+    removeValue(value){
+        let removed ;
+
+        if(this.isEmpty()){
+            return console.log("There is no value here")
+        }
+        else if(this.head.value === value){
+            removed= this.head
+            this.head = removed.next
+            this.size--
+            return value
+
+        }
+        else{
+            let prev = this.head;
+
+            while(prev.next && prev.next.value !== value){
+                prev = prev.next
+            }
+            if(prev.next){
+                removed= prev.next
+                prev.next = removed.next
+                this.size--
+                return value
+
+
+            }
+
+            return console.log("Value not found")
+    
+
+        }
+        
+
+    }
+    reverse(){
+        let prev = null
+        let curr = this.head
+        let next;
+
+        while(curr){
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        }
+
+        this.head = prev
+
+
+    }
+
+    print(){
+        if(this.isEmpty()){
+            console.log("This List is Empty")
+        } else{
+            let curr = this.head
+            let listValues = ''
+
+            while(curr){
+                listValues += `${curr.value} `
+                curr = curr.next
+            }
+
+            console.log(listValues)
+        }
+    }
+}
+
+const list = new LinkedList()
 
 
 
