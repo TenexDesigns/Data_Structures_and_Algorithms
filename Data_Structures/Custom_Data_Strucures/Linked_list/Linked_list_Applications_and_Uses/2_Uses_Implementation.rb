2cd70c71c95e52a37be34da6c154b8feda6ca545
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-28 16:42:09 +0300

    Create 2_Uses_Implementation.rb

---
+++ Data_Structures/Custom_Data_Strucures/Linked_list/Linked_list_Applications_and_Uses/2_Uses_Implementation.rb
@@ -0,0 +1,155 @@
+Linked lists have various applications in different areas of computer science. Here are a few examples of where you can apply linked lists:
+
+1.Image Viewer:
+Linked lists can be used in an image viewer where the images are ordered in a linked list, and we can traverse forward or backward to view the next or previous image. Heres a simple implementation of this idea:
+
+
+class Node {
+    constructor(image) {
+        this.image = image;
+        this.next = null;
+    }
+}
+
+class ImageViewer {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    addImage(image) {
+        let newNode = new Node(image);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+
+    displayImages() {
+        let current = this.head;
+        while (current) {
+            console.log(current.image);
+            current = current.next;
+        }
+    }
+}
+
+let viewer = new ImageViewer();
+viewer.addImage("Image1.jpg");
+viewer.addImage("Image2.jpg");
+viewer.addImage("Image3.jpg");
+viewer.displayImages();
+In this example, each Node represents an image, and the ImageViewer class maintains a linked list of images. The addImage method adds an image to the end of the list, and the displayImages method displays all images in the list freecodecamp.org.
+
+
+
+
+      
+2.Browser History:
+      A doubly linked list can be used to implement a browser history. The next pointer can be used to go forward in the history, and the previous pointer can be used to go back.
+
+        Heres a simple implementation:
+
+class Node {
+    constructor(url) {
+        this.url = url;
+        this.next = null;
+        this.previous = null;
+    }
+}
+
+class BrowserHistory {
+    constructor() {
+        this.current = new Node(null);
+    }
+
+    visit(url) {
+        let newNode = new Node(url);
+        newNode.previous = this.current;
+        this.current.next = newNode;
+        this.current = newNode;
+    }
+
+    back() {
+        if (this.current.previous !== null) {
+            this.current = this.current.previous;
+        }
+        return this.current.url;
+    }
+
+    forward() {
+        if (this.current.next !== null) {
+            this.current = this.current.next;
+        }
+        return this.current.url;
+    }
+}
+
+let browserHistory = new BrowserHistory();
+browserHistory.visit("www.google.com");
+browserHistory.visit("www.stackoverflow.com");
+console.log(browserHistory.back());  // Outputs: "www.google.com"
+console.log(browserHistory.forward());  // Outputs: "www.stackoverflow.com"
+In this example, each Node represents a visited URL, and the BrowserHistory class maintains a doubly linked list of visited URLs. The visit method visits a new URL, the back method goes back to the previous URL, and the forward method goes forward to the next URL codingninjas.com.
+
+
+
+
+  
+3.Music Playlist:
+  A circular linked list can be used to implement a music playlist where the songs keep playing in a loop. The next pointer of the last song points to the first song, creating a loop.
+Heres a simple implementation:
+
+class Node {
+    constructor(song) {
+        this.song = song;
+        this.next = null;
+    }
+}
+
+class MusicPlaylist {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    addSong(song) {
+        let newNode = new Node(song);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+            this.tail.next = this.head;
+        } else {
+            newNode.next = this.head;
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+
+    playSongs() {
+        let current = this.head;
+        do {
+            console.log(current.song);
+            current = current.next;
+        } while (current !== this.head);
+    }
+}
+
+let playlist = new MusicPlaylist();
+playlist.addSong("Song1.mp3");
+playlist.addSong("Song2.mp3");
+playlist.addSong("Song3.mp3");
+playlist.playSongs();
+
+      
+In this example, each Node represents a song, and the MusicPlaylist class maintains a circular linked list of songs. The addSong method adds a song to the end of the list, and the playSongs method plays all songs in the list in a loop dev.to.
+
+
+
+
+
+
+...
