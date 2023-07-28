a2f2cc58a35986b7168913671277dc2aeccd1e03
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-28 16:06:36 +0300

    Create Doublely_Linked_list_Implementation.rb

---
+++ Data_Structures/Custom_Data_Strucures/Linked_list/Doublely_Linked_list_Implementation.rb
@@ -0,0 +1,201 @@
+A doubly linked list is a type of linked list in which each node contains a data element and two pointers, next and previous, which point to the next and previous node in the sequence, respectively. This structure allows for more efficient operations in some cases, such as deleting a node or iterating backwards through the list, but it also requires more memory to store the additional references sebhastian.com.
+
+A Doubly Linked List is a type of linked list in which each node contains two pointers, one pointing to the next node (as in a singly linked list) and another pointing to the previous node. This extra pointer allows for traversal in both directions, forwards and backwards, making it more flexible than a singly linked list.
+The primary advantage of a doubly linked list over a singly linked list is that it can traverse in both directions. This can be particularly useful when you need to navigate back and forth through a list. In contrast, a singly linked list only allows for traversal in one direction, from the head to the tail.
+
+Each node in a Doubly Linked List has three fields:
+
+value: The value or data stored in the node.
+next: A pointer/reference to the next node in the list.
+prev: A pointer/reference to the previous node in the list.
+
+
+
+  Insertion in a Doubly Linked List:
+
+Insertion at the beginning: This operation involves adding a new node at the beginning of the list. It requires updating the next pointer of the new node and the prev pointer of the current head node.
+Insertion at the end: This operation involves adding a new node at the end of the list. It requires updating the next pointer of the current tail node and the prev pointer of the new node.
+Insertion at a given position: This operation involves adding a new node at a specific position in the list. It requires updating the next and prev pointers of the nodes before and after the new node.
+Deletion in a Doubly Linked List:
+
+Deletion from the beginning: This operation involves removing the head node from the list. It requires updating the next pointer of the new head node and setting the prev pointer of the new head node to null.
+Deletion from the end: This operation involves removing the tail node from the list. It requires updating the next pointer of the new tail node and setting the next pointer of the new tail node to null.
+Deletion from a given position: This operation involves removing a node from a specific position in the list. It requires updating the next and prev pointers of the nodes before and after the node being removed.
+
+
+  Advantages of Doubly Linked List over Singly Linked List:
+
+Bi-directional traversal: The presence of the prev pointer allows for easier traversal of the list in both forward and backward directions, which can be beneficial in certain algorithms and operations.
+Efficient deletion from the end: In a singly linked list, deleting the last node requires traversing the entire list to update the next pointer of the second-to-last node. In a doubly linked list, this operation is more efficient as you can directly access the previous node and update both next and prev pointers.
+Improved list reversal: Reversing a singly linked list requires additional memory or rebuilding the list. In a doubly linked list, you can efficiently reverse the list by swapping the next and prev pointers of each node.
+
+    However, its worth noting that a Doubly Linked List requires more memory to store the additional prev pointers, and updating the prev pointers during insertions and deletions requires additional overhead. In scenarios where bi-directional traversal is not necessary, a singly linked list might be more suitable due to its lower memory requirements and simpler operations. The choice between a singly linked list and a doubly linked list depends on the specific requirements and usage patterns of the data structure in a particular application.
+
+
+
+Sure, lets implement a Doubly Linked List in JavaScript and discuss its use and advantages over a singly linked list.
+
+// Node class for Doubly Linked List
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+// Doubly Linked List class
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  // Insert at the beginning of the list
+  prepend(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.size++;
+  }
+
+  // Insert at the end of the list
+  append(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.size++;
+  }
+
+  // Delete node from the beginning of the list
+  deleteFirst() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const removedNode = this.head;
+    this.head = this.head.next;
+    if (this.head) {
+      this.head.prev = null;
+    } else {
+      this.tail = null;
+    }
+    this.size--;
+    return removedNode.value;
+  }
+
+  // Delete node from the end of the list
+  deleteLast() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const removedNode = this.tail;
+    this.tail = this.tail.prev;
+    if (this.tail) {
+      this.tail.next = null;
+    } else {
+      this.head = null;
+    }
+    this.size--;
+    return removedNode.value;
+  }
+
+  // Check if the list is empty
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  // Get the size of the list
+  getSize() {
+    return this.size;
+  }
+
+  // Print the elements of the list from the head to tail
+  printForward() {
+    let curr = this.head;
+    let result = "";
+    while (curr) {
+      result += curr.value + " ";
+      curr = curr.next;
+    }
+    console.log(result);
+  }
+
+  // Print the elements of the list from the tail to head
+  printBackward() {
+    let curr = this.tail;
+    let result = "";
+    while (curr) {
+      result += curr.value + " ";
+      curr = curr.prev;
+    }
+    console.log(result);
+  }
+}
+
+// Usage example
+const dll = new DoublyLinkedList();
+
+dll.append(10);
+dll.append(20);
+dll.append(30);
+
+dll.prepend(5);
+dll.prepend(3);
+
+dll.printForward(); // Output: 3 5 10 20 30
+dll.printBackward(); // Output: 30 20 10 5 3
+
+dll.deleteFirst();
+dll.deleteLast();
+
+dll.printForward(); // Output: 5 10 20
+dll.printBackward(); // Output: 20 10 5
+Advantages of Doubly Linked List over Singly Linked List:
+
+Bi-directional traversal: The primary advantage of a Doubly Linked List is that it allows for efficient traversal in both forward and backward directions. This is useful in scenarios where bidirectional traversal is required, such as implementing undo/redo functionality or moving backward through a history of actions.
+Improved deletion at the end: In a Doubly Linked List, deleting the last element is more efficient since we can directly access the tail node and update the pointers without traversing the entire list.
+Efficient reversal: Reversing a Doubly Linked List is more efficient since it involves swapping the next and prev pointers, without requiring additional memory or rebuilding the list.
+However, its essential to consider the trade-offs. A Doubly Linked List requires more memory to store the additional prev pointers, and updating both next and prev pointers during insertions and deletions requires additional overhead. In scenarios where bidirectional traversal is not necessary, and memory usage is a concern, a singly linked list might be a better choice. The choice between a singly linked list and a doubly linked list depends on the specific requirements of the application and the operations it needs to perform on the data structure.
+
+
+
+
+
+INSERTION AND DELETION
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+Insertion and deletion in a doubly linked list are similar to those in a singly linked list, but with some additional steps to maintain the previous pointers. Here are the basic operations:
+
+
+Insertion: There are several places where a node can be inserted in a doubly linked list: at the beginning, at the end, or at a certain position in the list.
+
+Inserting at the beginning: Create a new node. Set the next pointer of the new node to the current head node, and set the previous pointer of the current head node to the new node. Update the head pointer to the new node.
+Inserting at the end: Create a new node. Set the previous pointer of the new node to the current tail node, and set the next pointer of the current tail node to the new node. Update the tail pointer to the new node.
+Inserting at a certain position: Navigate to the desired position in the list. Create a new node. Set the next pointer of the new node to the node currently at that position, and set the previous pointer of the new node to the node before that position. Update the next pointer of the node before that position to the new node, and update the previous pointer of the node at that position to the new node sebhastian.com.
+Deletion: Deletion in a doubly linked list can also occur at the beginning, at the end, or at a certain position in the list.
+
+Deleting at the beginning: Set the head pointer to the second node in the list, and set the previous pointer of the new head node to null.
+Deleting at the end: Set the tail pointer to the second last node in the list, and set the next pointer of the new tail node to null.
+Deleting at a certain position: Navigate to the desired position in the list. Update the next pointer of the node before that position to the node after that position, and update the previous pointer of the node after that position to the node before that position sebhastian.com.
+
+
+
+
+
+
+  
+
+
+  
