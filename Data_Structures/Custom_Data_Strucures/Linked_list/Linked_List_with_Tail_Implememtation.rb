3584f8a5bf5fafeb6855b843eeff10ba96d6dee6
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-28 15:52:58 +0300

    Create Linked_List_with_Tail_Implememtation.rb

---
+++ Data_Structures/Custom_Data_Strucures/Linked_list/Linked_List_with_Tail_Implememtation.rb
@@ -0,0 +1,183 @@
+A linked list with a tail pointer in JavaScript is a data structure where each node contains a reference to the subsequent node in the sequence, and the last node, known as the tail, has a reference to null. This signifies the end of the list freecodecamp.org.
+
+The primary advantage of a linked list with a tail pointer is that it allows for efficient appending of elements to the end of the list. If you know the last element, or the tail, of the list, you can append a new element in constant time, O(1), because you simply need to update the tails next reference to the new node and update the tail pointer to the new node stackoverflow.com.
+
+Without a tail pointer, appending to a linked list becomes a linear operation, O(n), because you have to traverse the entire list to find the last element. Prepending or inserting into the list remains a constant-time operation, O(1), whether or not a tail pointer is used stackoverflow.com.
+
+In terms of space complexity, adding a tail pointer to a linked list does not significantly increase the memory footprint of the data structure. The nodes of the list will be taking up memory regardless, and the tail is simply another variable that references one of these nodes.
+
+The use of a tail pointer can also simplify certain programming tasks. For example, it can be used to implement a queue data structure with constant-time enqueue and dequeue operations. A linked list with a tail pointer can efficiently implement the enqueue operation by adding elements to the end of the list, and the dequeue operation by removing elements from the head of the list.
+
+Here is a simple implementation of a singly linked list with a tail pointer in JavaScript:
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    append(data) {
+        let newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+}
+In this implementation, the LinkedList class maintains a head and tail pointer. The append method adds an element to the end of the list by creating a new node, updating the next reference of the current tail to the new node, and updating the tail pointer to the new node digitalocean.com.
+
+
+
+
+MORE EXPLANANTION
+************************************************************************************************************************************************************************************
+
+
+  Implementing a linked list with a tail means that you maintain a reference to the last node (tail) of the linked list in addition to the reference to the first node (head). In a standard singly linked list without a tail, if you want to append an element to the end of the list, you would have to traverse the entire list from the head to find the last node, which takes O(n) time complexity. With a tail reference, you can directly access the last node, making appending an element to the end of the list more efficient with O(1) time complexity.
+
+Heres an implementation of a linked list with tail in JavaScript:
+
+javascript
+Copy code
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedListWithTail {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    getSize() {
+        return this.size;
+    }
+
+    prepend(value) {
+        const node = new Node(value);
+        if (this.isEmpty()) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++;
+    }
+
+    append(value) {
+        const node = new Node(value);
+        if (this.isEmpty()) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.size++;
+    }
+
+    insert(value, index) {
+        if (index < 0 || index > this.size) {
+            return console.log("Index is out of bound");
+        } else if (index === 0) {
+            this.prepend(value);
+        } else if (index === this.size) {
+            this.append(value);
+        } else {
+            const node = new Node(value);
+            let prev = this.head;
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            node.next = prev.next;
+            prev.next = node;
+            this.size++;
+        }
+    }
+
+    removeFrom(index) {
+        let removedNode;
+        if (index < 0 || index >= this.size) {
+            return console.log("Index is out of bound");
+        } else if (index === 0) {
+            removedNode = this.head;
+            this.head = this.head.next;
+            if (this.size === 1) {
+                this.tail = null;
+            }
+        } else {
+            let prev = this.head;
+            for (let i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+            removedNode = prev.next;
+            prev.next = removedNode.next;
+            if (index === this.size - 1) {
+                this.tail = prev;
+            }
+        }
+        this.size--;
+        return removedNode.value;
+    }
+
+    print() {
+        if (this.isEmpty()) {
+            console.log("This List is Empty");
+        } else {
+            let curr = this.head;
+            let listValues = "";
+            while (curr) {
+                listValues += `${curr.value} `;
+                curr = curr.next;
+            }
+            console.log(listValues);
+        }
+    }
+}
+
+// Test the LinkedListWithTail
+const list = new LinkedListWithTail();
+list.append(10);
+list.append(20);
+list.append(30);
+list.prepend(5);
+list.insert(15, 2);
+list.print(); // Output: 5 10 15 20 30
+console.log(list.getSize()); // Output: 5
+Advantages of using a linked list with tail:
+
+Faster appending: Appending an element to the end of the list is faster with O(1) time complexity because you have direct access to the tail node.
+Faster access to the last element: You can directly access the last element using the tail reference, which can be useful in certain scenarios.
+Easier implementation of certain algorithms: Some algorithms require easy access to both the first and last elements of the list, and having a tail reference simplifies such implementations.
+Its worth noting that maintaining a tail reference comes with the cost of extra memory overhead to store the reference. Whether you use a linked list with or without a tail depends on the specific requirements of your application and the types of operations you frequently perform on the list. If you frequently need to append elements or access the last element, using a linked list with tail might be a more suitable choice. Otherwise, a standard linked list may suffice.
+
+
+
+
+
+
+
+  
+
+  
+
+..
