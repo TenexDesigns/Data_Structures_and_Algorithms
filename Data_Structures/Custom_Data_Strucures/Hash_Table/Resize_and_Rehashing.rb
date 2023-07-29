70ef2fbb752fbe95d1a3009ed97723d4ec0e882f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-29 13:12:21 +0300

    Update Resize_and_Rehashing.rb

--- Data_Structures/Custom_Data_Strucures/Hash_Table/Resize_and_Rehashing.rb
+++ Data_Structures/Custom_Data_Strucures/Hash_Table/Resize_and_Rehashing.rb
@@ -49,6 +49,29 @@ class HashTable {
 }
 
 
+      Yes, the resize() method in the given implementation performs both resizing and rehashing of the elements in the hash table.
+
+Heres what the resize() method does:
+
+It creates a new array with double the size of the current numBuckets. This is the resizing step, where the capacity of the hash table is increased to accommodate more elements efficiently.
+
+It then initializes the this.buckets property with the newly created array. At this point, the this.buckets array has empty buckets.
+
+The size property is reset to 0 because the elements will be rehashed and inserted into the newly resized hash table.
+
+It iterates through each bucket in the oldBuckets array (the original hash table). For each non-empty bucket, it performs rehashing by calling the set() method for each key-value pair in that bucket.
+
+The set() method (which performs rehashing) recalculates the new index for each key-value pair using the updated numBuckets and inserts the pair into the corresponding bucket in the newly resized hash table.
+
+By rehashing the elements, the hash table can maintain better distribution of keys across the buckets, reducing the likelihood of collisions and improving overall efficiency.
+
+Rehashing is a technique used to redistribute elements based on the updated hash table size and hash function. It helps to achieve a more balanced distribution of keys, ensuring that the load factor (ratio of elements to the number of buckets) is kept low, which is essential for efficient hash table operations. The process of rehashing allows the hash table to scale dynamically as more elements are added, ensuring that the hash table remains efficient even as the number of elements increases.
+
+
+
+
+
+
       In this example, the set method inserts a key-value pair into the hash table, and checks if the load factor (size / numBuckets) exceeds the MAX_LOAD_FACTOR (0.75). If it does, the resize method is called to create a new array that is twice the size of the current one. All existing entries are then rehashed and inserted into the new array using the set method.
 
 This approach ensures that the load factor of the hash table stays below the threshold, which helps maintain the performance of operations such as insertions and lookups scaler.com, rvbsanjose.github.io.
