8c1545cb909c91dc96c0059e2b997fb6c8213225
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-29 08:30:39 +0300

    Create 2_Hash_Table_Collisions.rb

---
+++ Data_Structures/Custom_Data_Strucures/Hash_Table/2_Hash_Table_Collisions.rb
@@ -0,0 +1,105 @@
+In a hash table, a collision occurs when two or more keys are hashed to the same index in the underlying array. Since the size of the array is limited, collisions are inevitable, especially when the number of elements (keys) in the hash table increases. Collisions can lead to data loss and degrade the performance of hash table operations. Therefore, it is essential to handle collisions effectively to maintain the integrity and efficiency of the hash table.
+
+Here are some common techniques to avoid collisions in hash tables:
+
+Separate Chaining:
+
+In separate chaining, each bucket (index) of the hash table contains a linked list or an array to store multiple key-value pairs that have the same hash code.
+When a collision occurs, the new key-value pair is added to the linked list or array at the corresponding bucket.
+This technique ensures that all colliding elements are stored together, allowing efficient retrieval during hash table operations.
+
+
+
+
+Open Addressing:
+
+Open addressing is another collision resolution technique where all elements are stored directly in the hash tables array without using separate data structures like linked lists.
+When a collision occurs, the algorithm probes through the array to find the next available slot to store the key-value pair.
+There are different probing methods, such as linear probing, quadratic probing, and double hashing, to determine the next available slot.
+
+
+
+
+Sure, here is how you can handle hash table collisions using separate chaining and open addressing in JavaScript:
+
+Separate Chaining:
+In separate chaining, each index of the hash table stores a list. When a collision occurs, the new key-value pair is added to the end of the list at the collided index.
+class HashTable {
+    constructor() {
+        this.table = [];
+    }
+    hashFunction(key) {
+        let sum = 0;
+        for (let i = 0; i < key.length; ++i) {
+            sum += key.charCodeAt(i);
+        }
+        return sum % 71;
+    }
+    put(key, value) {
+        let index = this.hashFunction(key);
+        if (this.table[index] === undefined) {
+            this.table[index] = [];
+        }
+        return this.table[index].push([key, value]);
+    }
+    get(key) {
+        let index = this.hashFunction(key);
+        for (let i = 0; i < this.table[index].length; i++) {
+            if (this.table[index][i][0] === key) {
+                return this.table[index][i][1];
+            }
+        }
+        return undefined;
+    }
+}
+jarednielsen.com
+
+
+
+
+
+
+
+      
+Open Addressing: 
+      In open addressing, when a collision occurs, we look for another open slot in the array to hold the value. One of the ways to do this is linear probing, where if a collision occurs at index hash(key), linear probing checks hash(key) + 1, hash(key) + 2, ..., hash(key) + i until an empty slot is found.
+class HashTable {
+    constructor() {
+        this.size = 10;
+        this.keys = new Array(this.size);
+        this.values = new Array(this.size);
+    }
+    hashFunction(key) {
+        let sum = 0;
+        for (let i = 0; i < key.length; ++i) {
+            sum += key.charCodeAt(i);
+        }
+        return sum % this.size;
+    }
+    put(key, value) {
+        let index = this.hashFunction(key);
+        while (this.keys[index] != null) {
+            if (this.keys[index] == key) {
+                this.values[index] = value;
+                return;
+            }
+            index = (index + 1) % this.size;
+        }
+        this.keys[index] = key;
+        this.values[index] = value;
+    }
+    get(key) {
+        let index = this.hashFunction(key);
+        while (this.keys[index] != key) {
+            index = (index + 1) % this.size;
+        }
+        return this.values[index];
+    }
+}
+Please note that the provided JavaScript code examples are very basic and are intended to illustrate the concept. In a real-world application, you would need to provide additional checks and handle edge cases to ensure the hash table works correctly.
+
+
+
+
+
+
