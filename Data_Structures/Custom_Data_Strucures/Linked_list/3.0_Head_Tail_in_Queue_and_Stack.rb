ba0808422c95b30f1ffb5501e2a8272ae3888e4b
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-29 08:28:50 +0300

    Update 3.0_Head_Tail_in_Queue_and_Stack.rb

--- Data_Structures/Custom_Data_Strucures/Linked_list/3.0_Head_Tail_in_Queue_and_Stack.rb
+++ Data_Structures/Custom_Data_Strucures/Linked_list/3.0_Head_Tail_in_Queue_and_Stack.rb
@@ -49,6 +49,130 @@ The choice of using prepend or append in the implementation of stack and queue i
 
 
 
+******************************************************************************************************************************************************
+
+        In the implementation of a linked list, the terms "head" and "start" are often used interchangeably to refer to the first node in the list, while "tail" and "finish" are used to refer to the last node in the list.
+
+When it comes to stacks and queues, they have different behaviors:
+
+A stack follows the LIFO (Last In First Out) principle. This means that the last item that is added (pushed) to the stack is the first one to be removed (popped). In terms of linked list implementation, you would typically add (push) and remove (pop) items at the head of the list. This is because adding or removing items at the head of a linked list can be done in constant time O(1), regardless of the size of the list. In this case, both the "push" operation (akin to "prepend") and the "pop" operation (akin to "remove from first") are performed at the head of the list stackoverflow.com, stackoverflow.com.
+class Stack:
+    def __init__(self):
+        self.head = None
+
+    def push(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def pop(self):
+        if self.head is None:
+            return None
+        popped = self.head.data
+        self.head = self.head.next
+        return popped
+A queue follows the FIFO (First In First Out) principle. This means that the first item that is added (enqueued) to the queue is the first one to be removed (dequeued). In terms of linked list implementation, you would typically add items at the tail of the list (akin to "append") and remove items at the head of the list (akin to "remove from first"). This is because adding items at the tail ensures that they will be removed after items that were added earlier, and removing items at the head can be done in constant time O(1) stackoverflow.com, stackoverflow.com.
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def enqueue(self, data):
+        new_node = Node(data)
+        if self.tail is not None:
+            self.tail.next = new_node
+        else:
+            self.head = new_node
+        self.tail = new_node
+
+    def dequeue(self):
+        if self.head is None:
+            return None
+        dequeued = self.head.data
+        self.head = self.head.next
+        if self.head is None:
+            self.tail = None
+        return dequeued
+The LIFO behavior of stacks and the FIFO behavior of queues are fundamental to their definitions and use cases, and they directly influence how the start (head) and finish (tail) of the linked list are used in each case.
+
+
+    ------------------------------------------------------------------------------------------------------------------------------------
+        In linked list implementations of stack and queue data structures, the head and tail of the list can be interpreted differently based on the context. Here's how they work:
+
+Stack: A stack uses the LIFO (Last In, First Out) principle. This means that the last element that was added to the stack is the first one to be removed. In a linked list implementation of a stack, the head of the list can be considered as the top of the stack. When you push an element onto the stack, you're essentially appending it at the start of the list (prepend operation). When you pop an element from the stack, you're removing it from the start of the list. The tail of the list is the bottom of the stack, but in typical stack operations, you don't interact with the tail.
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+  
+  // push operation
+  push(value) {
+    let node = new Node(value);
+    node.next = this.top;
+    this.top = node;
+  }
+  
+  // pop operation
+  pop() {
+    if (this.top) {
+      let value = this.top.value;
+      this.top = this.top.next;
+      return value;
+    }
+    return null;
+  }
+}
+geeksforgeeks.org
+
+Queue: A queue uses the FIFO (First In, First Out) principle. This means the first element that was added to the queue is the first one to be removed. In a linked list implementation of a queue, the tail of the list can be considered as the end of the queue where new elements are added (append operation), and the head of the list is the start of the queue where elements are removed.
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+  }
+
+  // enqueue operation
+  enqueue(value) {
+    let node = new Node(value);
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      this.last.next = node;
+      this.last = node;
+    }
+  }
+  
+  // dequeue operation
+  dequeue() {
+    if (this.first) {
+      let value = this.first.value;
+      this.first = this.first.next;
+      if (!this.first) {
+        this.last = null;
+      }
+      return value;
+    }
+    return null;
+  }
+}
+dev.to
+
+In both cases, the start of the list (head for stack, and both head and tail for queue) is influenced by the LIFO or FIFO principle, respectively, as these operations determine where new elements are added and where existing elements are removed.
 
 
 
