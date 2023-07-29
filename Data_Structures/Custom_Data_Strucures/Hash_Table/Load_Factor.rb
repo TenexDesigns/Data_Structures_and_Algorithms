6bf63a4fe85f1e75457b75e866fd055e2e2c57a1
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-07-29 11:08:41 +0300

    Update and rename Load_Facor.rb to Load_Factor.rb

--- Data_Structures/Custom_Data_Strucures/Hash_Table/Load_Factor.rb
+++ Data_Structures/Custom_Data_Strucures/Hash_Table/Load_Factor.rb
@@ -93,3 +93,52 @@ By dynamically resizing the hash table and rehashing the elements, we ensure tha
 
 
 
+
+  MORE EXPLANANTION
+**********************************************************************************************************************************************************
+    
+
+  The load factor in a hash table is a measure that determines when to increase the capacity of the hash table to maintain the efficiency of search and insert operations. It is defined as the ratio of the number of entries (m) to the total size of the hash table (n). The load factor can be calculated as m/n scaler.com.
+
+Maintaining a low load factor helps to reduce the likelihood of collisions. A collision occurs when two different keys hash to the same index in the hash table. If the load factor is high, the table may become cluttered, leading to longer search times and more collisions geeksforgeeks.org.
+
+In JavaScript, you can implement a hash table as a class and manage the load factor as follows:
+
+class HashTable {
+  constructor(initialCapacity = 8, loadFactor = 0.75) {
+    this.buckets = new Array(initialCapacity);
+    this.numBuckets = this.buckets.length;
+    this.size = 0;
+    this.MAX_LOAD_FACTOR = loadFactor;
+  }
+
+  // ... other methods ...
+
+  set(key, value) {
+    // ... code to add entry ...
+
+    if (this.size / this.numBuckets > this.MAX_LOAD_FACTOR) {
+      this.resize();
+    }
+  }
+
+  resize() {
+    const oldBuckets = this.buckets;
+    this.numBuckets *= 2; // double the size
+    this.buckets = new Array(this.numBuckets);
+    this.size = 0;
+
+    for (let i = 0; i < oldBuckets.length; i++) {
+      if (oldBuckets[i]) {
+        for (let j = 0; j < oldBuckets[i].length; j++) {
+          this.set(oldBuckets[i][j][0], oldBuckets[i][j][1]);
+        }
+      }
+    }
+  }
+}
+If the load factor exceeds the specified threshold (0.75 by default in this example), the set method triggers the resize method. The resize method doubles the size of the hash table and rehashes all the existing entries scaler.com.
+
+This process of increasing the size of the hash table and rehashing the elements is also known as rehashing. It helps to distribute the entries evenly across the expanded hash table, maintaining the efficiency of search and insert operations scaler.com.
+
+
