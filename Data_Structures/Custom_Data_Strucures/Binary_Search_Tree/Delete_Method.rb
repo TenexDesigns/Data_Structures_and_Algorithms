4ad82951f329c3f4d831d4275b1885c34b3b63e4
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-01 14:00:06 +0300

    Create Delete_Method.rb

---
+++ Data_Structures/Custom_Data_Strucures/Binary_Search_Tree/Delete_Method.rb
@@ -0,0 +1,149 @@
+
+
+
+
+Node with no children: If the node to be deleted has no children (leaf node), simply remove the node from the tree.
+
+Node with one child (either left or right): If the node to be deleted has only one child, connect its parent to its child, effectively removing the node from the tree.
+
+Node with two children: If the node to be deleted has two children, find the nodes in-order successor (the smallest value in the right subtree), replace the nodes value with the in-order successors value, and then delete the in-order successor node.
+
+
+
+
+
+
+class BinarySearchTree {
+    // ...
+
+    delete(value) {
+        this.root = this.deleteNode(this.root, value);
+    }
+
+    deleteNode(node, value) {
+        if (node === null) {
+            return null; // Node not found, do nothing.
+        }
+
+        if (value === node.value) {
+            // Case 1: Node has no children (Leaf Node)
+            if (node.left === null && node.right === null) {
+                return null;
+            }
+            // Case 2: Node has only one child
+            else if (node.left === null) {
+                return node.right;
+            } else if (node.right === null) {
+                return node.left;
+            }
+            // Case 3: Node has two children
+            else {
+                // Find the minimum node in the right subtree
+                let tempNode = this.findMinNode(node.right);
+                // Replace the value of the current node with the value of the minimum node
+                node.value = tempNode.value;
+                // Delete the minimum node in the right subtree
+                node.right = this.deleteNode(node.right, tempNode.value);
+                return node;
+            }
+        } else if (value < node.value) {
+            // If value is less than the current nodes value, traverse left subtree
+            node.left = this.deleteNode(node.left, value);
+            return node;
+        } else {
+            // If value is greater than the current nodes value, traverse right subtree
+            node.right = this.deleteNode(node.right, value);
+            return node;
+        }
+    }
+
+    findMinNode(node) {
+        if (node.left === null) {
+            return node;
+        } else {
+            return this.findMinNode(node.left);
+        }
+    }
+
+    // ...
+}
+
+
+
+
+
+
+The delete method in the BinarySearchTree class is used to delete a node with a specific value from the binary search tree. It works by calling the deleteNode method on the root of the tree with the value to be deleted.
+
+The deleteNode method is a recursive method that traverses the tree to find the node to be deleted and then deletes it according to one of three cases:
+
+Case 1: The node has no children. If the node to be deleted has no children (its a leaf node), it can be safely removed from the tree. This is done by returning null in place of the node, which effectively removes it from the tree.
+Case 2: The node has only one child. If the node to be deleted has only one child, the node can be removed and its child can take its place. This is done by returning the child node in place of the node to be deleted. If the node has only a right child (node.left === null), return node.right; is used to replace the node with its right child. If the node has only a left child (node.right === null), return node.left; is used to replace the node with its left child.
+Case 3: The node has two children. If the node to be deleted has two children, the situation is a bit more complex. The node cant be simply removed, as this would disconnect the two subtrees. Instead, the nodes value is replaced with the minimum value from the right subtree (which is found using the findMinNode method), and then this minimum value node is deleted from the right subtree. This ensures that the binary search tree property is maintained. This is done with the lines node.value = tempNode.value; and node.right = this.deleteNode(node.right, tempNode.value);. The updated node is then returned with return node;.
+
+----------------
+  The lines: 
+    "     // Delete the minimum node in the right subtree
+                node.right = this.deleteNode(node.right, tempNode.value);
+                return node;"
+    
+    Explanation:
+
+node.right = this.deleteNode(node.right, tempNode.value);: This line recursively calls the deleteNode method on the right subtree of the current node (node). The goal is to find and delete the minimum node in the right subtree.
+
+return node;: After deleting the minimum node in the right subtree, the current node (node) is returned. This is important because the current nodes value has been updated with the value of the minimum node (as a replacement), and its right subtree has been adjusted accordingly.
+  ------
+  
+  The lines node.right = this.deleteNode(node.right, value); and node.left = this.deleteNode(node.left, value); are used to recursively call the deleteNode method on the right or left child of the node, respectively. This is done when the value to be deleted is greater than or less than the current nodes value, respectively. The updated child node (which may have had a node deleted) is then assigned back to node.right or node.left.
+
+The return node; statements are used to return the updated node back up the recursive call stack. This allows the changes made to the tree (specifically, the deletion of the node) to be propagated back up to the root of the tree.
+
+The findMinNode method is a helper method used to find the node with the minimum value in a given subtree. Its used in the deleteNode method to find the minimum value node in the right subtree when the node to be deleted has two children. It works by recursively traversing to the left child of the node until it reaches a node with no left child, which is the minimum value node.
+
+
+
+
+
+
+
+
+
+
+
+
+======
+    The line node.right = this.deleteNode(node.right, tempNode.value); is part of the process of deleting a node that has two children in a binary search tree.
+
+When a node with two children is to be deleted, we can't just remove the node, as it would disconnect the two subtrees. So, we need to find a replacement for the node that maintains the binary search tree property. The replacement is chosen as the node with the smallest value that is larger than the current node's value, which is the node with the minimum value in the right subtree (tempNode).
+
+Once we have this tempNode, we replace the value of the node to be deleted with the value of tempNode (this is done in the line node.value = tempNode.value;).
+
+However, now we have a duplicate of tempNode in the tree (one at the position of the node we wanted to delete and one in the right subtree), so we need to delete tempNode from the right subtree. This is done with the line node.right = this.deleteNode(node.right, tempNode.value);. This line is a recursive call to deleteNode that deletes tempNode from the right subtree and returns the root of the updated subtree, which is then assigned back to node.right.
+
+Finally, return node; returns the updated node (which now has the value of tempNode and an updated right subtree that no longer contains tempNode) back up the recursive call stack. This allows the changes made to the tree to be propagated back up to the root of the tree.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
