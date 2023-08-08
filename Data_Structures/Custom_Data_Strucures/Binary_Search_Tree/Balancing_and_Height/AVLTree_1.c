4ac2bf55a70d9248f1f45b445754b35751128cc4
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-08 22:33:17 +0300

    Create AVLTree_1.c

---
+++ Data_Structures/Custom_Data_Strucures/Binary_Search_Tree/Balancing_and_Height/AVLTree_1.c
@@ -0,0 +1,208 @@
+An AVL tree (Adelson-Velsky and Landis tree) is a self-balancing binary search tree. It maintains a balanced structure by ensuring that the heights of the two child subtrees of any node differ by at most one. This balancing helps in maintaining efficient search, insertion, and deletion operations, all of which have a time complexity of O(log n) in an AVL tree.
+
+The balance factor of a node in an AVL tree is the difference between the heights of its left and right subtrees. It can have three possible values: -1, 0, or 1. A balance factor of -1 indicates that the left subtree is one level taller, a balance factor of 1 indicates that the right subtree is one level taller, and a balance factor of 0 indicates that the two subtrees are of the same height.
+
+Here are the four basic rotations used for balancing an AVL tree:
+
+1. Left Rotation (LL Rotation):
+   - Used when the balance factor of a node becomes greater than 1 and the imbalance is in the left subtree of the left child.
+   
+2. Right Rotation (RR Rotation):
+   - Used when the balance factor of a node becomes less than -1 and the imbalance is in the right subtree of the right child.
+   
+3. Left-Right Rotation (LR Rotation):
+   - Used when the balance factor of a node becomes greater than 1 and the imbalance is in the right subtree of the left child.
+   
+4. Right-Left Rotation (RL Rotation):
+   - Used when the balance factor of a node becomes less than -1 and the imbalance is in the left subtree of the right child.
+
+In the following descriptions, assume node A is the unbalanced node, and B, C, and D are its children.
+
+- Left Rotation (LL Rotation):
+```
+     A
+    / \
+   B   T3
+  / \
+ T1  C
+    / \
+   T2  T3
+```
+
+- Right Rotation (RR Rotation):
+```
+   A
+  / \
+ T1  B
+    / \
+   C  T3
+  / \
+ T2  T3
+```
+
+- Left-Right Rotation (LR Rotation):
+```
+   A
+  / \
+ T1  B
+    / \
+   T2  C
+       / \
+      T3  T4
+```
+
+- Right-Left Rotation (RL Rotation):
+```
+    A
+   / \
+  B  T1
+ / \
+C   T2
+   / \
+  T3  T4
+```
+
+When implementing AVL trees, you need to perform these rotations to maintain balance. Here's a basic JavaScript implementation of an AVL tree with insertion and the required rotations:
+
+```javascript
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.height = 1;
+    }
+}
+
+class AVLTree {
+    constructor() {
+        this.root = null;
+    }
+
+    getHeight(node) {
+        return node ? node.height : 0;
+    }
+
+    getBalanceFactor(node) {
+        return node ? this.getHeight(node.left) - this.getHeight(node.right) : 0;
+    }
+
+    leftRotate(y) {
+        const x = y.right;
+        const T2 = x.left;
+
+        x.left = y;
+        y.right = T2;
+
+        y.height = Math.max(this.getHeight(y.left), this.getHeight(y.right)) + 1;
+        x.height = Math.max(this.getHeight(x.left), this.getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    rightRotate(x) {
+        const y = x.left;
+        const T2 = y.right;
+
+        y.right = x;
+        x.left = T2;
+
+        x.height = Math.max(this.getHeight(x.left), this.getHeight(x.right)) + 1;
+        y.height = Math.max(this.getHeight(y.left), this.getHeight(y.right)) + 1;
+
+        return y;
+    }
+
+    insert(value) {
+        this.root = this._insert(this.root, value);
+    }
+
+    _insert(node, value) {
+        if (!node) {
+            return new TreeNode(value);
+        }
+
+        if (value < node.value) {
+            node.left = this._insert(node.left, value);
+        } else if (value > node.value) {
+            node.right = this._insert(node.right, value);
+        } else {
+            return node; // Duplicate values not allowed
+        }
+
+        node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
+
+        const balance = this.getBalanceFactor(node);
+
+        // Left Heavy
+        if (balance > 1) {
+            if (value < node.left.value) {
+                return this.rightRotate(node);
+            } else {
+                node.left = this.leftRotate(node.left);
+                return this.rightRotate(node);
+            }
+        }
+
+        // Right Heavy
+        if (balance < -1) {
+            if (value > node.right.value) {
+                return this.leftRotate(node);
+            } else {
+                node.right = this.rightRotate(node.right);
+                return this.leftRotate(node);
+            }
+        }
+
+        return node;
+    }
+}
+```
+
+Please note that this code provides a basic implementation of AVL tree insertion and rotations. Deletion and other features are not included in this code for simplicity.
+
+Remember that while AVL trees guarantee a balanced structure, other self-balancing trees like Red-Black trees might be preferred in certain scenarios due to their slightly relaxed balancing conditions, which can result in faster insertion and deletion operations in practice.
+
+
+
+
+
+
+
+------------------------------------------------------
+
+
+
+
+An AVL tree (Adelson-Velsky and Landis tree) is a self-balancing binary search tree in which the difference of heights of left and right subtrees of any node is less than or equal to one. This property ensures that the tree remains approximately balanced, leading to O(log n) search times, where n is the number of nodes in the tree [Source 0](https://www.freecodecamp.org/news/avl-tree-insertion-rotation-and-balance-factor/), [Source 6](https://www.educba.com/avl-tree-rotation/), [Source 7](https://www.baeldung.com/java-avl-trees).
+
+**Balance Factor**
+
+The balance factor of a node in an AVL tree is the height of the left subtree minus the height of the right subtree. It can be -1, 0, or 1. If the balance factor of any node is not in this range, a rotation operation is needed to balance the tree [Source 6](https://www.educba.com/avl-tree-rotation/).
+
+* If the balance factor is -1, the left subtree is one level higher than the right subtree.
+* If the balance factor is 0, the left and right subtrees are at the same level.
+* If the balance factor is 1, the left subtree is one level lower than the right subtree.
+
+**Rotations**
+
+There are four types of rotations in AVL trees: Left rotation, Right rotation, Left-Right rotation, and Right-Left rotation [Source 6](https://www.educba.com/avl-tree-rotation/).
+
+1. **Left Rotation (LL)**: This rotation is performed when a right child is inserted into the right subtree of the right child of the unbalanced node, or a node is deleted from the left subtree. The unbalanced node and its right child are involved in the rotation.
+
+2. **Right Rotation (RR)**: This rotation is performed when a left child is inserted into the left subtree of the left child of the unbalanced node, or a node is deleted from the right subtree. The unbalanced node and its left child are involved in the rotation.
+
+3. **Left-Right Rotation (LR)**: This rotation is a combination of Left rotation followed by Right rotation. It is performed when a right child is inserted into the left subtree of the left child of the unbalanced node, or a node is deleted from the right subtree.
+
+4. **Right-Left Rotation (RL)**: This rotation is a combination of Right rotation followed by Left rotation. It is performed when a left child is inserted into the right subtree of the right child of the unbalanced node, or a node is deleted from the left subtree.
+
+Unfortunately, implementing AVL trees in JavaScript is quite complex and beyond the scope of this response. However, you can find a full implementation in the [GeeksforGeeks AVL Tree tutorial](https://www.geeksforgeeks.org/avl-tree-set-1-insertion/).
+
+
+
+
+
+
+
+
+  
