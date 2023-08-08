7b316f2ebddbf719242a9bfad1ff0fa2bb068a9e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-08 08:34:28 +0300

    Update Recursion.c

--- Recursion/Recursion.c
+++ Recursion/Recursion.c
@@ -49,4 +49,194 @@ Several classic programming problems are often solved using recursion:
 
 7. **Backtracking Problems:** Many combinatorial optimization problems, like the N-Queens problem or Sudoku, can be solved using recursion and backtracking.
 
-It's important to note that while recursion is a powerful technique, it might not always be the most efficient solution. In some cases, an iterative or more specialized approach might be preferable.
+Its important to note that while recursion is a powerful technique, it might not always be the most efficient solution.
+  In some cases, an iterative or more specialized approach might be preferable.
+
+
+
+
+
+THE ABOVE MENTIOND PROBLEMS ARE SOLVED HERE
+----------------------------------------------------------------------------------------------------------------
+  Certainly! Ill provide code samples and explanations for each of the mentioned problems that are solved using recursion.
+
+1. **Fibonacci Sequence:**
+   Problem: Calculate the \(n\)th Fibonacci number.
+   
+   Solution:
+   ```javascript
+   function fibonacci(n) {
+     if (n <= 0) {
+       return 0; // Base case for 0th Fibonacci number
+     } else if (n === 1) {
+       return 1; // Base case for 1st Fibonacci number
+     } else {
+       return fibonacci(n - 1) + fibonacci(n - 2); // Recursive case
+     }
+   }
+   ```
+
+   Reasoning: Each Fibonacci number is the sum of the two preceding numbers. Recursion is used to break down the problem into simpler subproblems until the base cases are reached.
+
+2. **Factorials:**
+   Problem: Calculate the factorial of \(n\).
+   
+   Solution:
+   ```javascript
+   function factorial(n) {
+     if (n <= 1) {
+       return 1; // Base case
+     } else {
+       return n * factorial(n - 1); // Recursive case
+     }
+   }
+   ```
+
+   Reasoning: The factorial of \(n\) is the product of all positive integers from 1 to \(n\). Recursion is used to repeatedly reduce the problem size by multiplying by \(n\) and decrementing \(n\).
+
+3. **Binary Tree Traversal:**
+   Problem: Traverse a binary tree in in-order, pre-order, or post-order.
+   
+   Solution:
+   ```javascript
+   class TreeNode {
+     constructor(value) {
+       this.value = value;
+       this.left = null;
+       this.right = null;
+     }
+   }
+
+   function inOrderTraversal(root) {
+     if (root !== null) {
+       inOrderTraversal(root.left);
+       console.log(root.value);
+       inOrderTraversal(root.right);
+     }
+   }
+   ```
+
+   Reasoning: Recursion is used to explore left and right subtrees of each node in a binary tree in a specific order.
+
+4. **Maze Solving:**
+   Problem: Find a path to solve a maze or navigate through a grid.
+   
+   Solution:
+   ```javascript
+   function solveMaze(maze, row, col) {
+     if (row < 0 || row >= maze.length || col < 0 || col >= maze[0].length || maze[row][col] === '#') {
+       return false; // Base case: out of bounds or blocked cell
+     }
+     if (maze[row][col] === 'E') {
+       return true; // Base case: reached the exit
+     }
+     maze[row][col] = '#'; // Mark cell as visited
+     return solveMaze(maze, row + 1, col) || // Move down
+            solveMaze(maze, row - 1, col) || // Move up
+            solveMaze(maze, row, col + 1) || // Move right
+            solveMaze(maze, row, col - 1);   // Move left
+   }
+   ```
+
+   Reasoning: Recursion is used to explore all possible paths in a maze or grid until the exit is reached or all options are exhausted.
+
+5. **String Permutations:**
+   Problem: Generate all possible permutations of a string.
+   
+   Solution:
+   ```javascript
+   function permutations(str) {
+     if (str.length === 0) {
+       return ['']; // Base case: empty string
+     }
+     const result = [];
+     for (let i = 0; i < str.length; i++) {
+       const char = str[i];
+       const remaining = str.slice(0, i) + str.slice(i + 1);
+       const perms = permutations(remaining);
+       for (const perm of perms) {
+         result.push(char + perm);
+       }
+     }
+     return result;
+   }
+   ```
+
+   Reasoning: Recursion is used to generate permutations by selecting each character one by one and recursively generating permutations for the remaining characters.
+
+6. **Tower of Hanoi:**
+   Problem: Solve the Tower of Hanoi puzzle, moving disks between pegs.
+   
+   Solution:
+   ```javascript
+   function towerOfHanoi(n, source, auxiliary, target) {
+     if (n === 1) {
+       console.log(`Move disk 1 from ${source} to ${target}`);
+       return;
+     }
+     towerOfHanoi(n - 1, source, target, auxiliary);
+     console.log(`Move disk ${n} from ${source} to ${target}`);
+     towerOfHanoi(n - 1, auxiliary, source, target);
+   }
+   ```
+
+   Reasoning: Recursion is used to break down the problem into smaller subproblems, moving \(n-1\) disks to an auxiliary peg before moving the \(n\)th disk to the target peg.
+
+7. **Backtracking Problems:**
+   Problem: Solve combinatorial optimization problems, such as the N-Queens problem or Sudoku.
+   
+   Solution (N-Queens):
+   ```javascript
+   function solveNQueens(n) {
+     const board = Array.from({ length: n }, () => Array(n).fill('.'));
+     const solutions = [];
+
+     function isSafe(row, col) {
+       for (let i = 0; i < row; i++) {
+         if (board[i][col] === 'Q') return false; // Check column
+         if (col - row + i >= 0 && board[i][col - row + i] === 'Q') return false; // Check left diagonal
+         if (col + row - i < n && board[i][col + row - i] === 'Q') return false; // Check right diagonal
+       }
+       return true;
+     }
+
+     function backtrack(row) {
+       if (row === n) {
+         solutions.push([...board.map(row => row.join(''))]);
+         return;
+       }
+       for (let col = 0; col < n; col++) {
+         if (isSafe(row, col)) {
+           board[row][col] = 'Q';
+           backtrack(row + 1);
+           board[row][col] = '.';
+         }
+       }
+     }
+
+     backtrack(0);
+     return solutions;
+   }
+   ```
+
+   Reasoning: Backtracking problems involve exploring multiple possibilities and recursively exploring paths while maintaining constraints. N-Queens is an example where recursion is used to place queens on a chessboard such that no two queens threaten each other.
+
+Recursion is a powerful technique for solving a wide range of problems, especially those that can be broken down into smaller instances of the same problem or those that involve exploring multiple paths. It mirrors the inherent structure of many problems, making it an elegant and efficient approach in these cases.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
