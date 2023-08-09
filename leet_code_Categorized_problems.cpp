f975c221638e3b1d7e11919f09ed27307185e58f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-09 15:28:17 +0300

    Create leet_code_Categorized_problems.cpp

---
+++ leet_code_Categorized_problems.cpp
@@ -0,0 +1,45 @@
+As of my last knowledge update in September 2021, there isn't an official categorization of LeetCode problems into specific data structures' needs (like easy, medium, and hard categories) on the LeetCode platform itself. However, LeetCode does provide tags for each problem that indicate relevant topics and data structures, which can help you identify problems based on your preferred data structures. These tags can be found on the problem page.
+
+For example, a problem might have tags like "Array," "Linked List," "Tree," "Graph," "Dynamic Programming," "Hash Table," and so on. You can use these tags to filter problems and focus on specific data structures or algorithms that you want to practice.
+
+Additionally, the LeetCode community often creates curated lists, blog posts, and GitHub repositories that categorize problems based on specific data structures, algorithms, or concepts. These resources can help you find problems that match your practice needs.
+
+Here's how you might approach finding problems based on data structures:
+
+1. **LeetCode Tags**: When you're browsing problems on LeetCode, pay attention to the tags listed for each problem. These tags indicate the relevant data structures and concepts. You can filter problems by these tags to find problems related to specific data structures.
+
+2. **Curated Lists and Repositories**: Search online for curated lists or GitHub repositories that categorize LeetCode problems based on data structures. Many developers and educators create these resources to help others navigate LeetCode. For example, you might find lists like "Top 100 LeetCode Problems by Category" or "LeetCode Patterns" that categorize problems based on their underlying concepts.
+
+3. **Online Communities**: Join online coding communities, forums, or discussion platforms where programmers share their LeetCode solutions and study plans. These communities often provide recommendations and guidelines for practicing specific data structures and algorithms.
+
+4. **Educational Platforms**: Some educational platforms offer guided LeetCode courses that categorize problems by difficulty and data structure. These courses provide structured learning paths and explanations for solving LeetCode problems.
+
+Remember that LeetCode frequently adds new problems and features, so it's a good idea to check the platform directly and explore any new features or categorization methods that might have been introduced since my last knowledge update.
+
+Always make sure to read problem statements, understand the requirements, and strive to understand the underlying concepts rather than focusing solely on solving problems mechanically. This approach will help you develop a deeper understanding of data structures and algorithms.
+
+
+
+
+WHERE TO FIND CATEGORIZED LEET CODE PROBLEMS
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+As of my last knowledge update in September 2021, there isn't an official categorization of LeetCode problems into specific data structures' needs (like easy, medium, and hard categories) on the LeetCode platform itself. However, LeetCode does provide tags for each problem that indicate relevant topics and data structures, which can help you identify problems based on your preferred data structures. These tags can be found on the problem page.
+
+For example, a problem might have tags like "Array," "Linked List," "Tree," "Graph," "Dynamic Programming," "Hash Table," and so on. You can use these tags to filter problems and focus on specific data structures or algorithms that you want to practice.
+
+Additionally, the LeetCode community often creates curated lists, blog posts, and GitHub repositories that categorize problems based on specific data structures, algorithms, or concepts. These resources can help you find problems that match your practice needs.
+
+Here's how you might approach finding problems based on data structures:
+
+1. **LeetCode Tags**: When you're browsing problems on LeetCode, pay attention to the tags listed for each problem. These tags indicate the relevant data structures and concepts. You can filter problems by these tags to find problems related to specific data structures.
+
+2. **Curated Lists and Repositories**: Search online for curated lists or GitHub repositories that categorize LeetCode problems based on data structures. Many developers and educators create these resources to help others navigate LeetCode. For example, you might find lists like "Top 100 LeetCode Problems by Category" or "LeetCode Patterns" that categorize problems based on their underlying concepts.
+
+3. **Online Communities**: Join online coding communities, forums, or discussion platforms where programmers share their LeetCode solutions and study plans. These communities often provide recommendations and guidelines for practicing specific data structures and algorithms.
+
+4. **Educational Platforms**: Some educational platforms offer guided LeetCode courses that categorize problems by difficulty and data structure. These courses provide structured learning paths and explanations for solving LeetCode problems.
+
+Remember that LeetCode frequently adds new problems and features, so it's a good idea to check the platform directly and explore any new features or categorization methods that might have been introduced since my last knowledge update.
+
+Always make sure to read problem statements, understand the requirements, and strive to understand the underlying concepts rather than focusing solely on solving problems mechanically. This approach will help you develop a deeper understanding of data structures and algorithms.
