22f8cb068e11ed777d4cae04e5bcca84a120edef
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-11 08:06:15 +0300

    Create 2_learn_using_Pseudo_Code.java

---
+++ 1_Learning/2_learn_using_Pseudo_Code.java
@@ -0,0 +1,16 @@
+If you prefer to learn data structures and algorithms using pseudocode and a problem-solving approach, there are resources available that can help you with that. Here are a few recommendations:
+
+1. "Grokking Algorithms" by Aditya Bhargava - This book uses a problem-solving approach to teach algorithms and data structures. It explains concepts using pseudocode and focuses on understanding the problem-solving techniques behind the algorithms.
+
+2. "Algorithm Design Manual" by Steven S. Skiena - This book provides a practical approach to algorithm design and analysis. It includes pseudocode implementations and emphasizes problem-solving strategies.
+
+3. "Introduction to the Design and Analysis of Algorithms" by Anany Levitin - This book introduces algorithms and data structures using pseudocode. It covers various problem-solving techniques and provides examples and exercises to reinforce the concepts.
+
+4. HackerRank (www.hackerrank.com) - This platform offers a wide range of coding challenges, including algorithmic problems. It allows you to solve problems using pseudocode and provides a discussion forum where you can learn from others' solutions.
+
+When learning with a pseudocode-first approach, it is not necessary to know all data structures off the top of your head. Instead, focus on understanding the problem-solving techniques and the principles behind different data structures and algorithms. As you practice solving problems and implementing algorithms in pseudocode, you will gain a deeper understanding of how to apply them to real-world scenarios.
+
+Remember, the goal is to develop problem-solving skills and the ability to analyze and design efficient algorithms. With practice and experience, you will become more comfortable with different data structures and their applications.
+
+
+  ..
