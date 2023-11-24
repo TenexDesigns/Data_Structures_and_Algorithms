68e363bbbb2919a622eb1f96b9faa8dfc5a5d9f1
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-24 18:19:51 +0300

    Create Bucket Js The Third Part DS tool.java

---
+++ 1.3_Use third-party libraries for DSA/Bucket Js The Third Part DS tool.java
@@ -0,0 +1,90 @@
+Certainly! Below are the basic properties, methods, and performance characteristics for some of the data structures provided by Buckets-JS:
+
+### Set:
+- **Basic Properties:**
+  - Collection of unique elements.
+- **Methods:**
+  - `add(element)`: Adds an element to the set.
+  - `contains(element)`: Checks if the set contains a specific element.
+  - `remove(element)`: Removes an element from the set.
+  - `union(set)`: Performs the union of two sets.
+- **Performance Characteristics:**
+  - Generally O(1) for add, remove, and contains operations.
+
+### Dictionary:
+- **Basic Properties:**
+  - Collection of key-value pairs.
+- **Methods:**
+  - `add(key, value)`: Adds a key-value pair to the dictionary.
+  - `get(key)`: Retrieves the value associated with a key.
+  - `remove(key)`: Removes a key-value pair from the dictionary.
+  - `containsKey(key)`: Checks if a key exists in the dictionary.
+- **Performance Characteristics:**
+  - Generally O(1) for add, remove, and get operations.
+
+### Heap:
+- **Basic Properties:**
+  - Binary tree with parent-child relationships based on element values.
+- **Methods:**
+  - Operations for maintaining heap properties.
+- **Performance Characteristics:**
+  - O(log n) for insertion and extraction of the minimum/maximum element.
+
+### Queue:
+- **Basic Properties:**
+  - Collection with FIFO (First In, First Out) order.
+- **Methods:**
+  - `enqueue(element)`: Adds an element to the end of the queue.
+  - `dequeue()`: Removes and returns the front element of the queue.
+- **Performance Characteristics:**
+  - O(1) for enqueue and dequeue operations.
+
+### Stack:
+- **Basic Properties:**
+  - Collection with LIFO (Last In, First Out) order.
+- **Methods:**
+  - `push(element)`: Adds an element to the top of the stack.
+  - `pop()`: Removes and returns the top element of the stack.
+- **Performance Characteristics:**
+  - O(1) for push and pop operations.
+
+### LinkedList:
+- **Basic Properties:**
+  - Linear collection of elements.
+- **Methods:**
+  - `add(element)`: Adds an element to the list.
+  - `remove(element)`: Removes an element from the list.
+- **Performance Characteristics:**
+  - O(1) for add and remove operations.
+
+### BSTree (Binary Search Tree):
+- **Basic Properties:**
+  - Tree structure with sorted elements.
+- **Methods:**
+  - `add(element)`: Adds an element to the tree.
+  - `remove(element)`: Removes an element from the tree.
+  - `contains(element)`: Checks if the tree contains a specific element.
+- **Performance Characteristics:**
+  - O(log n) for search, insertion, and deletion operations.
+
+These are just summaries, and the actual performance may vary based on specific scenarios and usage patterns. It's recommended to refer to the library documentation for more detailed information.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
