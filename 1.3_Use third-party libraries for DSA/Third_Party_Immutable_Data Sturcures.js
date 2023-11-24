2d688cd04621886bfa56f81a73391c652a1b9728
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-24 14:41:56 +0300

    Create Third_Party_Immutable_Data Sturcures.js

---
+++ 1.3_Use third-party libraries for DSA/Third_Party_Immutable_Data Sturcures.js
@@ -0,0 +1,41 @@
+Here are some of the most popular third-party libraries for data structures in JavaScript and TypeScript:
+
+* **TypeORM:** A popular ORM (Object-Relational Mapping) library for TypeScript and JavaScript. It provides a powerful and easy-to-use API for working with databases.
+[Image of TypeORM library for data structures in JavaScript and TypeScript]
+
+* **Lodash:** A popular JavaScript library that provides a wide range of utility functions, including functions for working with arrays, objects, and strings.
+[Image of Lodash library for data structures in JavaScript and TypeScript]
+
+* **Ramda:** A popular JavaScript library that provides a collection of pure functional programming functions.
+[Image of Ramda library for data structures in JavaScript and TypeScript]
+
+* **immutable.js:** A popular JavaScript library that provides immutable data structures.
+[Image of immutable.js library for data structures in JavaScript and TypeScript]
+
+* **underscore:** A popular JavaScript library that provides a wide range of utility functions, similar to Lodash.
+[Image of underscore library for data structures in JavaScript and TypeScript]
+
+* **RxJS:** A popular JavaScript library for reactive programming. It provides a powerful and flexible API for working with asynchronous data streams.
+[Image of RxJS library for data structures in JavaScript and TypeScript]
+
+These are just a few of the many popular third-party libraries for data structures in JavaScript and TypeScript. The best library for you will depend on your specific needs and preferences.
+
+Here is a table that summarizes the data structures and methods supported by some of the popular libraries:
+
+| Library | Data Structures | Methods |
+|---|---|---|
+| TypeORM | Entities, relationships, queries | Create, read, update, delete (CRUD) |
+| Lodash | Arrays, objects, strings | Map, filter, reduce, find, sort |
+| Ramda | Arrays, objects, functions | Map, filter, reduce, find, sort |
+| immutable.js | Lists, maps, sets | Immutable versions of standard data structures |
+| underscore | Arrays, objects, strings, functions | Similar to Lodash |
+| RxJS | Streams, observables | Subscribe, pipe, map, filter, reduce |
+
+I hope this helps!
+
+
+
+
+
+
+
