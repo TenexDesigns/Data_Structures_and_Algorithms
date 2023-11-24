024b6393a667344b2a795bb5cfdc95f3e80de609
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-24 10:26:19 +0300

    Update 1_No Need to Implemet Data Strucres From Scratch.java

--- 1.0_No Need to Implemet Data Strucres From Scratch/1_No Need to Implemet Data Strucres From Scratch.java
+++ 1.0_No Need to Implemet Data Strucres From Scratch/1_No Need to Implemet Data Strucres From Scratch.java
@@ -1,7 +1,9 @@
 Absolutely, in programming, you don't always need to implement data structures from scratch to use them. 
 
-  Use third-party libraries for DSA,  or built-in or library implementations of common data structures and focus on solving the specific problem at hand
-    However, it is still important to have a basic understanding of the underlying principles of data structures and algorithms, as this will help you to choose the right library for your project and use it effectively.
+ --> You don't always need to implement data structures from scratch to use them
+ --> Use third-party libraries for DSA,  or built-in or library implementations of common data structures and focus on solving the specific problem at hand
+ --> However, it is still important to have a basic understanding of the underlying principles of data structures and algorithms, as this will help you to choose the right library for your project and use it effectively.
+ --> (understand their basic properties, usage patterns, and performance characteristics,Focus on problem-solving strategie,Understand data structure usage patterns,Break down complex problems into smaller ones, practice)
 
    
    Most programming languages, including TypeScript, Java, and Python, provide built-in or library implementations of common data structures.
