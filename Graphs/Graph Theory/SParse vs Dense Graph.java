daad411273c8a98931da7d0ddec3c84772034f7c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-08 15:46:00 +0300

    Create SParse vs Dense Graph.java

---
+++ Graphs/Graph Theory/SParse vs Dense Graph.java
@@ -0,0 +1,30 @@
+Desne Grapth - WHere each node is connnected to evry other node  in the graph
+  e.g Here  1 is connected to 2,3,4,  and the same can be said for the other node
+
+  1 ----------2
+  | \         |
+  |   \       |
+  |     \     |
+  |       \   |
+  |         \ |   
+  3 ----------4
+
+    
+Sparse Graph  -  where  the nodes are not connected to all the other nodes in the graph
+
+
+  1           2
+  | \         |
+  |   \       |
+  |     \     |
+  |       \   |
+  |         \ |   
+  3           4 
+  
+
+
+
+
+
+
+
