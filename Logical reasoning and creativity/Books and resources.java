5acacc9272da53697454c62cd120588ff04f8c70
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-15 11:34:18 +0300

    Create Books and resources.java

---
+++ Logical reasoning and creativity/Books and resources.java
@@ -0,0 +1,64 @@
+Bat ares mathematicl olympiad -   https://www.bamo.org/archives/examfiles/bamo99draft.pdf
+
+
+
+
+
+Resources to Enhance Your Skills:
+
+Books:
+"Structure and Interpretation of Computer Programs" by Harold Abelson and Gerald Jay Sussman
+"Think Like a Programmer: An Introduction to Creative Problem Solving" by V. Anton Spraul
+"Algorithms + Data Structures = Programs" by Rod Stephens
+Online Courses:
+"Programming: Puzzles for Beginners" by Google on Coursera
+"Introduction to Algorithms" by MIT OpenCourseware
+"Learn to Code by Solving Problems" by HackerRank
+Websites:
+"Project Euler" (https://projecteuler.net/) - Solve challenging mathematical problems with code.
+"HackerRank" (https://www.hackerrank.com/) - Practice coding challenges and compete with others.
+"Interview Cake" (https://www.interviewcake.com/) - Prepare for technical interviews with coding problems.
+Remember:
+
+
+
+
+ "Cracking the Coding Interview" by Gayle Laakmann McDowell, "The Art of Computer Programming" by Donald E. Knuth, and "Introduction to Algorithms" by Thomas H. Cormen, Charles E. Leiserson, Ronald L. Rivest, and Clifford Stein
+
+
+
+"Cracking the Coding Interview" by Gayle Laakmann McDowell is a popular resource for technical interview preparation.
+"Algorithmic Problem Solving" by Roland Backhouse provides insights into solving algorithmic problems.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
